447300b2e0f8542719274d3463cb45847a285232
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-11 12:34:31 +0200

    Add collection

--- apps/marketplace-fullscreen/pages/index.vue
+++ apps/marketplace-fullscreen/pages/index.vue
@@ -1,8 +1,13 @@
 <template lang="pug">
 VList(flex-grow="1" min-h="0" pos="relative" p="6" space-y="6")
   h1(text="3xl" font="bold") MY ASSETS
-  div(grid="~ cols-2 md:cols-3 lg:cols-4 2xl:cols-5" divide-x="0.5")
-    ListItem(:token="token" v-for="token in allTokens" b="gray-400")
+
+  HList(space-x="3")
+    VList(w="150")
+      h3(font="bold" uppercase="~") Collection
+
+    div(grid="~ cols-2 md:cols-3 lg:cols-4 2xl:cols-5" divide-x="0.5")
+      ListItem(:token="token" v-for="token in allTokens" b="gray-400")
 
 </template>
 
