4c2be60dcce2596742a65ec22ec41d321d66e1d0
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-16 11:45:47 +0200

    Fix space x

--- apps/marketplace-fullscreen/components/Collection/Header/Index.vue
+++ apps/marketplace-fullscreen/components/Collection/Header/Index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-HList(py="3" pos="sticky top-16" bg="gray-800" z="2")
+HList(py="3" pos="sticky top-16" space-x="3" bg="gray-800" z="2")
   CollectionHeaderSearchBar()
   CollectionHeaderToggleDisplay()
 
