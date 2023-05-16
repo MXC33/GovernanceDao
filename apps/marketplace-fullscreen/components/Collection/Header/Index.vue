e9d9eeb8abb6e80707486257edd0a3c6d44a2ac1
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-16 11:42:24 +0200

    FIx z for collection header

--- apps/marketplace-fullscreen/components/Collection/Header/Index.vue
+++ apps/marketplace-fullscreen/components/Collection/Header/Index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-HList(py="3" pos="sticky top-16" bg="gray-800")
+HList(py="3" pos="sticky top-16" bg="gray-800" z="2")
   CollectionHeaderSearchBar()
   CollectionHeaderToggleDisplay()
 
