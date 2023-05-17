130831345d9f6583a117317ec62f60475daf4e0c
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-17 15:52:20 +0200

    Better search hits

--- apps/marketplace-fullscreen/components/Collection/Filter/Index.vue
+++ apps/marketplace-fullscreen/components/Collection/Filter/Index.vue
@@ -3,8 +3,10 @@ HList(py="3" pos="sticky top-16" space-x="3" bg="ix-black" z="2")
   Search(:options="items" :search-paths="['type', 'tier']" w="full")
     template(#item="{item}")
       HList(items="center" space-x="3")
-        TokenImage(:token="item" w="12")
-        TokenName(:token="item" capitalize="~")
+        div(w="12")
+          TokenImage(:token="item"  :key="'img' + getTokenKey(item)")
+
+        TokenName(:token="item" capitalize="~" :key="'name' + getTokenKey(item)")
 
   CollectionFilterToggleDisplay()
 
@@ -12,8 +14,8 @@ HList(py="3" pos="sticky top-16" space-x="3" bg="ix-black" z="2")
 
 <script lang="ts" setup>
 import type { CollectionItem } from '~/composables/useCollection';
-
-const props = defineProps<{
+const { getTokenKey } = useTokens()
+defineProps<{
   items: CollectionItem[]
 }>()
 
