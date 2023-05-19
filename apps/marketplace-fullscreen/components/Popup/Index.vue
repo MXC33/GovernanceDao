663a0ca934b6c44e80b446dc187b90a855ab3fef
Philip Wallner <philipwallner@outlook.com>
2023-05-19 15:14:54 +0200

    margin = auto

--- apps/marketplace-fullscreen/components/Popup/Index.vue
+++ apps/marketplace-fullscreen/components/Popup/Index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 //-PopupWrapper()
-div(pos="sticky top-50%" bg="black opacity-80%" rounded="5" m="35%" z="100" b="~ red" backdrop="blur")
-  VList(p="5" items="center" capitalized="~")
+div(pos="sticky top-45%" bg="black opacity-80%" rounded="5" m="auto" z="100" b="~ red" backdrop="blur")
+  VList(p="5" items="center" capitalized="~" space-y="5")
     slot()
-
+    //- w="100" h="100" top-50% left-50%
 </template>
