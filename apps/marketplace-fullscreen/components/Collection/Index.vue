874431bc0af59b8498c998fe64f83cb98887938c
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-05-19 17:42:53 +0200

    small fixes

--- apps/marketplace-fullscreen/components/Collection/Index.vue
+++ apps/marketplace-fullscreen/components/Collection/Index.vue
@@ -13,21 +13,21 @@ VList(flex-grow="1" min-h="0" pos="relative" p="8" space-y="6")
     Transition(name="slide-left")
       ContentDrawerWrapper(v-if="showFilters" pos="sticky top-58" h="100" inset="0")
 
-    Transition(name="fade" mode="out-in")
+    //- Transition(name="fade" mode="out-in")
       
+    //-   CollectionGrid(v-if="displayType == 'grid'")
+    //-     CollectionGridItem(:token="token" v-for="token in items" b="gray-400")
+
+    Transition(name="fade" mode="out-in" v-if="data")
       CollectionGrid(v-if="displayType == 'grid'")
-        CollectionGridItem(:token="token" v-for="token in items" b="gray-400")
+        CollectionGridItem(:token="token" v-for="token in data.nfts" b="gray-400")
 
-  Transition(name="fade" mode="out-in" v-if="data")
-    CollectionGrid(v-if="displayType == 'grid'")
-      CollectionGridItem(:token="token" v-for="token in data.nfts" b="gray-400")
-
-    CollectionTable(:columns="columns" :rows="data.nfts" v-else initial-sort="name")
-      template(#item-asset="{row}")
-        HList(items="center" space-x="2" font="bold")
-          div(w="12" h="12")
-            TokenImage(:token="row" w="12" h="12" :key="row.id")
-          TokenName(:token="row" capitalize="~")
+      CollectionTable(:columns="columns" :rows="data.nfts" v-else initial-sort="name")
+        template(#item-asset="{row}")
+          HList(items="center" space-x="2" font="bold")
+            div(w="12" h="12")
+              TokenImage(:token="row" w="12" h="12" :key="row.id")
+            TokenName(:token="row" capitalize="~")
 
 </template>
 
