4b749b028ad40f755e6d5bf4349cba72f63cd570
Ceronfred <122262506+Ceronfred@users.noreply.github.com>
2023-05-19 16:57:05 +0200

    updated json and transfer text
    Added to the text of the transfer composable is using json file.

--- apps/marketplace-fullscreen/components/Transfer/Index.vue
+++ apps/marketplace-fullscreen/components/Transfer/Index.vue
@@ -1,11 +1,16 @@
 <template lang="pug">
-div(text="2xl red") Trasfer Asset
+div(text="2xl red") {{ $t(`marketplaceFullscreen.transfer.title`) }}
 VList(items="center" p="3")
-  div(text="red") Quantity  
-  HelperAdjustable(text="2xl")
-div(text="red") You own 26 of this item
+    div(text="red") {{ $t(`marketplaceFullscreen.transfer.quantity`) }}
+    HelperAdjustable(text="2xl")
+div(text="red") {{ $t(`marketplaceFullscreen.transfer.own1`) }} 26 {{ $t(`marketplaceFullscreen.transfer.own2`) }} 
 VList(items="center" space-y="3")
-  div(text="2xl red") Wallet Adress
-  Input(text="2xl red" bg="black" placeholder="Put Adress Here" )
-  button(rounded="1" text="2xl black" bg="red") Confirm Adress
+    div(text="2xl red") {{ $t(`marketplaceFullscreen.transfer.walletAdress`) }}
+    Input(text="xl red" bg="black" placeholder="Put Adress Here..." )
+    button(rounded="1" text="2xl black" bg="red") {{ $t(`marketplaceFullscreen.transfer.confirm`) }}
 </template>
+
+<script setup lang="ts">
+//const { t } = useI18n()
+
+</script>
