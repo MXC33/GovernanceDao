491feb5a8b5ec9e37679f0a000f8d918e57d8d5e
Philip Wallner <philipwallner@outlook.com>
2023-05-19 14:43:40 +0200

    Folder restructure

--- apps/marketplace-fullscreen/components/Helper/Adjustable/Index.vue
+++ apps/marketplace-fullscreen/components/Helper/Adjustable/Index.vue
@@ -3,7 +3,7 @@ HList(flex-grow="1" space-x="1" w="full")
   HelperAdjustableButton(type="minus")
 
   HList(flex-grow="1" w="full" justify="center" b="1 $mc-secondary_40 on-inline:0" items="center" pos="relative")
-    //-HelperAdjustableNumber()
+    HelperAdjustableNumber()
 
   HelperAdjustableButton(type="plus")
 
--- apps/marketplace-fullscreen/components/Popup/Index.vue
+++ apps/marketplace-fullscreen/components/Popup/Index.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
+//-PopupWrapper()
 div(pos="sticky top-50%" bg="black opacity-80%" rounded="5" m="35%" z="100" b="~ red" backdrop="blur")
-  VList(p="10" items="center" capitalized="~")
+  VList(p="5" items="center" capitalized="~")
     slot()
 
 </template>
