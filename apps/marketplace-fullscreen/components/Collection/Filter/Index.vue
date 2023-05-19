ae724519cf7eaa3fb0a3e36871b39ee4aa88526f
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-05-19 09:44:14 +0200

    UI update
    Content Drawer
    Input Radio
    Input Checkbox
    Drawer Wrapper
    Filter Index
    Page Header Index

--- apps/marketplace-fullscreen/components/Collection/Filter/Index.vue
+++ apps/marketplace-fullscreen/components/Collection/Filter/Index.vue
@@ -1,4 +1,5 @@
 <template lang="pug">
+HList(:b="borderOnScroll" pos="sticky top-34" z="3" ml="-8" mr="-8")
 HList(w="full" pos="sticky top-34" z="2")
   HList(w="full" py="3" space-x="3" bg="ix-black")
 
@@ -22,6 +23,14 @@ const { getTokenKey } = useTokens()
 defineProps<{
   items: CollectionItem[]
 }>()
+
+const { y } = useWindowScroll()
+
+const borderOnScroll = computed(() => {
+  if (y.value >= 550)
+    return 'b-1 gray-600'
+  return '0'
+})
 
 </script>
 
--- apps/marketplace-fullscreen/components/ContentDrawer/index.vue
+++ apps/marketplace-fullscreen/components/ContentDrawer/index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 VList()
   VList(h="12" max-w="94" bg="ix-black" justify="center")
-    HList(uppercase="~" items="center" justify="between" px="8" font="bold" cursor="pointer" @click.stop="dropDrawer")
+    HList(uppercase="~" items="center" justify="between" p="r-8" font="bold" cursor="pointer" @click.stop="dropDrawer")
       span(color="white" text="md")
         slot(name="header")
       ChevronIcon(w="5" translate-y="-0.35" fill="white" transition="opacity" :selected="drawerState" rotate="on-selected:-180deg")
@@ -12,7 +12,7 @@ VList()
     HList(uppercase="~" v-if="drawerState")
       slot()
 
-  div(b="b-1 gray-600" bg="black")
+div(b="b-1 gray-600" bg="black" ml="-8")
 
 </template>
 
