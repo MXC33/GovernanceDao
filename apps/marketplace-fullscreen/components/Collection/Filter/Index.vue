3ad5c5850faac8c3d58cca2aee935c364fc9d139
Philip Wallner <philipwallner@outlook.com>
2023-05-19 09:42:39 +0200

    forgot publish

--- apps/marketplace-fullscreen/components/Collection/Filter/Index.vue
+++ apps/marketplace-fullscreen/components/Collection/Filter/Index.vue
@@ -1,14 +1,27 @@
+<!-- <script lang="ts" setup>
+const showFilter = ref(false);
+
+const toggleFilterBar = () => {
+  showFilter.value = !showFilter.value
+  console.log("toggleFilterBar");
+}
+</script> -->
+<script lang="ts" setup>
+const toggleFilterBar = () => {
+  emit('toggleFilter')
+  console.log("toggleFilterBar");
+}
+const emit = defineEmits(['toggleFilter']);
+</script>
+
 <template lang="pug">
 HList(w="full" pos="sticky top-34" z="2")
   HList(w="full" py="3" space-x="3" bg="ix-black")
-    CollectionFilterToggleFilter()
+    //- CollectionFilterToggleFilter(@toggleFilter="toggleFilterBar" )
+    CollectionFilterToggleFilter(@toggleFilter="toggleFilterBar" )
     CollectionFilterSearchBar()
     CollectionFilterToggleDisplay()
     
 </template>
-
-<script lang="ts">
-</script>
-
 
 
--- apps/marketplace-fullscreen/components/Collection/Filter/ToggleFilter/Index.vue
+++ apps/marketplace-fullscreen/components/Collection/Filter/ToggleFilter/Index.vue
@@ -1,8 +1,6 @@
 <template lang="pug">
 HList(uppercase="~" font="bold" text="white" frame="~" flex-shrink="0" w="12" h="12" items="center" justify="center")
-  CollectionFilterToggleFilterButton()
-    FilterIcon(w="5")
-
+  FilterIcon(p="4" @click="emit('toggleFilter')")
 </template>
   
 
@@ -10,5 +8,5 @@ HList(uppercase="~" font="bold" text="white" frame="~" flex-shrink="0" w="12" h=
 import FilterIcon from '~/assets/icons/filter.svg'
 
 const { displayType, toggleDisplayType } = useCollectionSettings()
-
+const emit = defineEmits(['toggleFilter']);
 </script>
