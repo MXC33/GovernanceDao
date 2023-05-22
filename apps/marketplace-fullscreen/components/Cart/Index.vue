9f06f7e9c4fc0bd10cc34305c4ca72d1f52493b0
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-22 11:42:13 +0200

    Fix z-index

--- apps/marketplace-fullscreen/components/Cart/Index.vue
+++ apps/marketplace-fullscreen/components/Cart/Index.vue
@@ -4,7 +4,7 @@ Transition(name="fade")
 
 Transition(name="slide-right")
   VList(pos="fixed right-0 top-0 bottom-0" overflow-y="auto" bg="ix-black" w="80 md:120" max-w="80%" z="200" color="white" ref="cartElement" v-if="viewingCart")
-    header(pos="sticky top-0" px="6" space-y="3" bg="black" p="t-6 b-3")
+    header(pos="sticky top-0" px="6" space-y="3" bg="black" p="t-6 b-3" z="1")
       h3(text="xl" font="bold") My Cart
 
       div(b="b-0.5 gray-600" mx="-6")
