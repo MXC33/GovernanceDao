a42dedd09cc5fe00ec54501cf6084c9ccce6c3b8
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-22 14:53:26 +0200

    Fix double filters

--- apps/marketplace-fullscreen/components/Collection/Index.vue
+++ apps/marketplace-fullscreen/components/Collection/Index.vue
@@ -6,9 +6,6 @@ VList(flex-grow="1" min-h="0" pos="relative" p="8" space-y="6")
     template(#attributes)
       AttributeList(:data="data" v-if="data" )
 
-  CollectionFilter(@toggle-filter="toggleFilterDrawer" :items="data.nfts" :filters="data.filters")
-      AttributeList()
-
   CollectionFilter(:items="data.nfts" :filters="data.filters" v-if="data"  @toggle-filter="toggleFilterDrawer")
 
   HList(pos="sticky")
@@ -35,7 +32,7 @@ import type { CollectionData } from '~/composables/useCollection';
 import type { TableColumn } from '~/composables/useTable'
 const { displayType } = useCollectionSettings()
 const { getTokenKey } = useTokens()
-const {cartItems} = useCart()
+const { cartItems } = useCart()
 const columns: TableColumn[] = [
   { label: "Asset", value: "name" },
   { label: "Higher bid price", value: "higher_bid_price", sortable: true },
