1627e3872bddb843db0cb7de25e378da5c875277
Ceronfred <122262506+Ceronfred@users.noreply.github.com>
2023-05-22 12:58:19 +0200

    Update Index.vue

--- apps/marketplace-fullscreen/components/Transfer/Index.vue
+++ apps/marketplace-fullscreen/components/Transfer/Index.vue
@@ -1,25 +1,25 @@
 <template lang="pug">
 VList(items="center" p="3")
-  div(text="2xl black" v-html="$t(`marketplaceFullscreen.transfer.title`)")
-  HList(space-x="3")
-    Image(w="10" h="10")
-    VList()
-      div(text="black") Title 
-      div(text="black") Planet IX  
-    div(flex="grow")
-    div(text="black" v-html="$t(`marketplaceFullscreen.transfer.own`)")
+    div(text="2xl black" v-html="$t(`marketplaceFullscreen.transfer.title`)")
+    HList(space-x="3")
+        Image(w="10" h="10")
+        VList()
+            div(text="black") Title 
+            div(text="black") Planet IX  
+        div(flex="grow")
+        div(text="black" v-html="$t(`marketplaceFullscreen.transfer.own`)")
 
 div(text="black") Quantity
-HelperAdjustable(text="2xl")
+Adjustable(v-model="Image" h="10")
 
 //-VList(items="center" space-y="3")
 div(text="2xl black") Wallet Adress
 Input(text="2xl black center" bg="white" placeholder="e.g Dx0000" border="")
 div()
-  div(text="black" v-html="$t(`marketplaceFullscreen.transfer.warningText`)")
+    div(text="black" v-html="$t(`marketplaceFullscreen.transfer.warningText`)")
 HList()
-  button(m="auto" text="2xl black" bg="gray") 
-  div(text="black" v-html="$t(`marketplaceFullscreen.transfer.verifyText`)")
+    button(m="auto" text="2xl black" bg="gray") 
+    div(text="black" v-html="$t(`marketplaceFullscreen.transfer.verifyText`)")
 
 button(m="auto" text="2xl black" bg="gray" p="2") Transfer Item 
 </template>
