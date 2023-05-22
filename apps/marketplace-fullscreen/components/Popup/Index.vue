be865c8f71f0d0d9d1db782f0e47b2632ab4b903
Philip Wallner <philipwallner@outlook.com>
2023-05-22 09:27:09 +0200

    Color change to figma style

--- apps/marketplace-fullscreen/components/Popup/Index.vue
+++ apps/marketplace-fullscreen/components/Popup/Index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 //-PopupWrapper()
-div(pos="sticky top-45%" bg="black opacity-80%" rounded="5" m="auto" z="100" b="~ red" backdrop="blur")
-  VList(p="5" items="center" capitalized="~" space-y="5")
+div(pos="sticky top-45%" bg="white opacity-80%" m="auto" z="100" b="~ red" backdrop="blur")
+  VList(p="5" capitalized="~" space-y="5")
     slot()
     //- w="100" h="100" top-50% left-50%
 </template>
--- apps/marketplace-fullscreen/components/Transfer/Index.vue
+++ apps/marketplace-fullscreen/components/Transfer/Index.vue
@@ -1,16 +1,11 @@
 <template lang="pug">
-div(text="2xl red") {{ $t(`marketplaceFullscreen.transfer.title`) }}
 VList(items="center" p="3")
-    div(text="red") {{ $t(`marketplaceFullscreen.transfer.quantity`) }}
-    HelperAdjustable(text="2xl")
-div(text="red") {{ $t(`marketplaceFullscreen.transfer.own1`) }} 26 {{ $t(`marketplaceFullscreen.transfer.own2`) }} 
-VList(items="center" space-y="3")
-    div(text="2xl red") {{ $t(`marketplaceFullscreen.transfer.walletAdress`) }}
-    Input(text="xl red" bg="black" placeholder="Put Adress Here..." )
     button(rounded="1" text="2xl black" bg="red") {{ $t(`marketplaceFullscreen.transfer.confirm`) }}
 </template>
 
 <script setup lang="ts">
 //const { t } = useI18n()
 
+<script lang="ts" setup>
+const { t } = useI18n()
 </script>
