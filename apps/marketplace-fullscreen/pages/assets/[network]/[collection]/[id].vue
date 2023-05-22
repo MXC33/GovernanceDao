2cc694b4951abbb5fb6c134cabf48cef48cad176
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-22 14:02:50 +0200

    Clean out unused page

--- apps/marketplace-fullscreen/pages/assets/[network]/[collection]/[id].vue
+++ apps/marketplace-fullscreen/pages/assets/[network]/[collection]/[id].vue
@@ -1,70 +1,9 @@
 <template lang="pug">
-Collection(:items="rows")
-  CollectionItemCollapsable(class="basis-1 md:basis-2/3" :title="'Listings'")
-    template(#icon)
-      Plus
-
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-    p ajhsdjkash askjdhas kdjaskh asjkhdka
-
+div not implemented
+//- Collection(:items="rows")
+//-   CollectionItemCollapsable(class="basis-1 md:basis-2/3" :title="'Listings'")
+//-     template(#icon)
+//-       Plus
 </template>
 
 
@@ -76,9 +15,9 @@ import Plus from '~/assets/icons/plus.svg'
 const { allTokens, fetchUserInventory } = useUserData()
 await fetchUserInventory()
 
-const rows = computed<IXToken[]>(() =>
-  allTokens.value.map((token) => token as IXToken)
-)
+// const rows = computed<IXToken[]>(() =>
+//   allTokens.value.map((token) => token as IXToken)
+// )
 
 
 </script>
