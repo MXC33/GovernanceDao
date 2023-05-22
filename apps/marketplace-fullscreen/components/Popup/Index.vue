817a5a1b1e23eb0c7dbc1272f8c9198fba9ef074
Philip Wallner <philipwallner@outlook.com>
2023-05-22 14:46:15 +0200

    DarkModeChange

--- apps/marketplace-fullscreen/components/Popup/Index.vue
+++ apps/marketplace-fullscreen/components/Popup/Index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 //-PopupWrapper()
-div(pos="sticky top-45%" bg="white opacity-80%" m="auto" z="100" b="~ red" backdrop="blur")
-  VList(p="5" capitalized="~" space-y="5")
-    slot()
+div(pos="sticky top-50%" bg="black opacity-80%" m="auto" z="100" b="~" backdrop="blur")
+  //-VList(p="5" capitalized="~" space-y="5")
+  slot()
     //- w="100" h="100" top-50% left-50%
 </template>
--- apps/marketplace-fullscreen/components/Transfer/Index.vue
+++ apps/marketplace-fullscreen/components/Transfer/Index.vue
@@ -1,30 +1,39 @@
 <template lang="pug">
-VList(items="center" p="3")
-    div(text="2xl black" v-html="$t(`marketplaceFullscreen.transfer.title`)")
-    HList(space-x="3")
-        Image(w="10" h="10")
-        VList()
-            div(text="black") Title 
-            div(text="black") Planet IX  
-        div(flex="grow")
-        div(text="black" v-html="$t(`marketplaceFullscreen.transfer.own`)")
+VList(p="5" capitalized="~" space-y="5")
+  VList(items="center" p="3")
+      div(text="2xl white" v-html="$t(`marketplaceFullscreen.transfer.title`)")
+      HList(space-x="3")
+          Image(w="10" h="10" b="~")
+          TokenImage(:token="token" pos="absolute" inset="0" object="contain center" :key="getTokenKey(token)")
+          VList()
+              div(text="white") Title 
+              div(text="white") Planet IX  
+          div(flex="grow")
+          div(text="white" v-html="$t(`marketplaceFullscreen.transfer.own`)")
 
-div(text="black") Quantity
-Adjustable(v-model="Image" h="10")
+  div(text="white") Quantity
+  Adjustable(v-model="Image" h="10")
 
-//-VList(items="center" space-y="3")
-div(text="black") Wallet Adress
-Input(text="2xl black center" bg="white" placeholder="e.g Dx0000" border="")
-div()
-    div(text="black" v-html="$t(`marketplaceFullscreen.transfer.warningText`)")
-HList(space-x="3")
-  FormCheckbox(v-model="isChecked") 
-  div(text="black" v-html="$t(`marketplaceFullscreen.transfer.verifyText`)")
+  //-VList(items="center" space-y="3")
+  div(text="white") Wallet Adress
+  Input(text="2xl white center" bg="white" placeholder="e.g Dx0000" border="")
+  div()
+      div(text="white" v-html="$t(`marketplaceFullscreen.transfer.warningText`)")
+  HList(space-x="3")
+    FormCheckbox(v-model="isChecked") 
+    div(text="white" v-html="$t(`marketplaceFullscreen.transfer.verifyText`)")
 
-button(m="auto" text="2xl black" bg="gray" p="2" v-if="isChecked") Transfer Item 
+  button(m="auto" text="2xl white" bg="gray" p="2" v-if="isChecked") Transfer Item 
 </template>
 
 <script lang="ts" setup>
 import Image from "~/assets/icons/plus.svg"
+import type { IXToken } from '@ix/base/composables/Token/useIXToken';
+
+const { getTokenKey } = useTokens()
+
 const isChecked = ref(false)
+const props = defineProps<{
+  token: IXToken,
+}>()
 </script>
