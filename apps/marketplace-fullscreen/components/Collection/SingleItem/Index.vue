6aec9a6a2fc3fd04efeefb1ea8055c07045f63e2
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-05-23 17:30:07 +0200

    icon

--- apps/marketplace-fullscreen/components/Collection/SingleItem/Index.vue
+++ apps/marketplace-fullscreen/components/Collection/SingleItem/Index.vue
@@ -6,13 +6,18 @@ div(grid="~ cols-8 gap-3" px="6")
 
       ContentDrawer(:start-open="true" v-if="item.description")
         template(#titleicon)
-          TitleWithIcon(icon="description") description
+          TitleWithIcon(icon="description")
+            template(#default) description
+
+
         template(#default) 
           div(p="3 t-0") {{ item.description }}
 
       ContentDrawer(:start-open="true")
         template(#titleicon)
-          TitleWithIcon(icon="traits") traits
+          TitleWithIcon(icon="traits")
+            template(#default) traits
+
         template(#default) 
           div(grid="~ cols-3 gap-3" pb="3")
             AttributeTrait(v-for="trait in item.attributes")
@@ -31,8 +36,8 @@ div(grid="~ cols-8 gap-3" px="6")
 
       ContentDrawer(:start-open="true")
         template(#titleicon)
-          TitleWithIcon(icon="listing") listing
-
+          TitleWithIcon(icon="listing")
+            template(#default) listing
         template(#default)
           Table(:columns="saleColumns" :rows="item.sales" id="single-item" :in-drawer="true")
             template(#item-action="{row}")
@@ -41,7 +46,8 @@ div(grid="~ cols-8 gap-3" px="6")
 
       ContentDrawer(:start-open="true")
         template(#titleicon)
-          TitleWithIcon(icon="offer") offers
+          TitleWithIcon(icon="offer")
+            template(#default) offer
         template(#default)
           Table(:columns="offerColumns" :rows="item.bids" id="offers" :in-drawer="true")
 
