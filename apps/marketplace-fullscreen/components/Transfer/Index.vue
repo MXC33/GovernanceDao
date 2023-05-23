0258c8098c76150fda6f55e63da2fe3b19c536da
Philip Wallner <philipwallner@outlook.com>
2023-05-23 11:29:33 +0200

    Removed repeded white text

--- apps/marketplace-fullscreen/components/Transfer/Index.vue
+++ apps/marketplace-fullscreen/components/Transfer/Index.vue
@@ -1,43 +1,69 @@
 <template lang="pug">
-Popup()
+Popup(text="white")
     VList(p="5" capitalized="~" space-y="5" b="~" rounded="2")
         //-items="center"
         VList(p="3") 
+<<<<<<< Updated upstream
             div(text="2xl white" v-html="$t(`mpFullscreen.transfer.title`)")
+=======
+            div(text="2xl" v-html="$t(`marketplaceFullscreen.transfer.title`)")
+>>>>>>> Stashed changes
             HList(space-x="3")
                 HList()
                     TokenImage(:token="collectionData.nfts[0]" inset="0" w="15" h="15" object="contain center" :key="getTokenKey(collectionData.nfts[0])")
                     VList()
-                        div(text="white") {{getTokenName(collectionData.nfts[0])}} 
-                        div(text="white") Collection name  
+                        div() {{getTokenName(collectionData.nfts[0])}} 
+                        div() Collection name  
                 div(flex="grow")
                 VList(items="center")
+<<<<<<< Updated upstream
                     div(text="white" v-html="$t(`mpFullscreen.transfer.own`)") 
                     div(text="white") 7          
 
         div(text="white" v-html="$t(`mpFullscreen.transfer.quantity`)")
         Adjustable(v-model="Image" h="10")
+=======
+                    div(v-html="$t(`marketplaceFullscreen.transfer.own`)") 
+                    div() {{hardCodedMax}}          
+
+        div() Quantity
+        //-h="10"
+        Adjustable(v-model="item" )
+>>>>>>> Stashed changes
 
         //-VList(items="center" space-y="3")
-        div(text="white") Wallet Adress
-        input(text="2xl white center" bg="black" placeholder="e.g Dx0000" border="" v-model="wallet" @input="onChange")
+        div() Wallet Adress
+        input(text="2xl center" bg="black" placeholder="e.g Dx0000" border="" v-model="wallet" @input="onChange")
         div()
+<<<<<<< Updated upstream
             div(text="white" v-html="$t(`mpFullscreen.transfer.warningText`)")
         Transition(name="slide-top")
             HList(space-x="3" v-if="wallet")
                 FormCheckbox(v-model="isChecked") 
                 div(text="white" v-html="$t(`mpFullscreen.transfer.verifyText`)")
+=======
+            div(v-html="$t(`marketplaceFullscreen.transfer.warningText`)")
+        Transition(name="slide-top")
+            HList(space-x="3" v-if="wallet")
+                FormCheckbox(v-model="isChecked") 
+                div(v-html="$t(`marketplaceFullscreen.transfer.verifyText`)")
+>>>>>>> Stashed changes
 
         Transition(name="slide-top")
             button(m="auto" text="~" bg="gray-600" b="~" p="2" w="full" v-if="isChecked && wallet" @click="itemTransfer" v-html="$t(`mpFullscreen.transfer.transferItem`)")
 </template>
 
 <script lang="ts" setup>
+import type { TransferItem } from '~/composables/useTransfer'
 import Image from "~/assets/icons/plus.svg"
 import type { IXToken } from '@ix/base/composables/Token/useIXToken';
 import type { AnyToken } from '@ix/base/composables/Token/useTokens';
 import type { CollectionData } from '~/composables/useCollection';
+//import type { CartItem } from '~/composables/useCart'
 
+//const item = defineModel<CartItem>()
+
+const item = defineModel<TransferItem>()
 const props = defineProps<{
     collectionData: CollectionData,
 }>()
@@ -48,6 +74,8 @@ const { transferNFT } = useTransferNFT()
 const wallet = ref("")
 const oldWalletAdress = ref("")
 const isChecked = ref(false)
+
+const hardCodedMax = 10
 
 const onChange = () => {
     if (wallet != oldWalletAdress) {
---
+++ apps/marketplace-fullscreen/composables/useTransfer.ts
@@ -0,0 +1,40 @@
+import { IXToken } from "@ix/base/composables/Token/useIXToken"
+import { AdjustableNumber } from "@ix/base/composables/Utils/useAdjustableNumber"
+
+export interface TransferItem extends AdjustableNumber {
+  token: IXToken,
+}
+
+export const useTransfer = () => {
+  const transferItem = useState<TransferItem[]>('transfer-items', () => [])
+  const viewingTransferItem = useState('transfer-visible', () => false)
+
+  const removeFromTransferList = (token: IXToken) => {
+
+    const index = transferItem.value.findIndex((item) => item.token.token_id == token.token_id)
+    console.log("remove", index, token.token_id, transferItem.value.map((item) => item.token.token_id))
+
+    transferItem.value.splice(index, 1)
+  }
+
+  const addToTransferList = (token: IXToken) => {
+    transferItem.value.push({
+      token,
+      value: 1
+    })
+
+    viewingTransferItem.value = true
+  }
+
+  const clearTransferList = () => {
+    transferItem.value = []
+  }
+
+  return {
+    transferItem,
+    viewingTransferItem,
+    removeFromTransferList,
+    addToTransferList,
+    clearTransferList
+  }
+}
