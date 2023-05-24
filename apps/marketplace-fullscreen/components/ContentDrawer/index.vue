7dc9257568370f593cf7f05a85442744aa8d0848
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-24 09:39:05 +0200

    Fix collapsable tables

--- apps/marketplace-fullscreen/components/ContentDrawer/index.vue
+++ apps/marketplace-fullscreen/components/ContentDrawer/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-VList(max-h="80" overflow-y="auto" frame="~")
+VList(max-h="80" frame="~")
   VList(overflow-y="auto" h="full" pos="relative")
     HList(h="12" px="3" bg="ix-black" items="center" justify="between" pos="sticky top-0" flex-shrink="0" z="1" @click.stop="dropDrawer" uppercase="~" cursor="pointer")
       span(color="white" text="md" font="bold" v-if="$slots.header")
@@ -11,7 +11,7 @@ VList(max-h="80" overflow-y="auto" frame="~")
       ChevronIcon(w="5" translate-y="-0.35" fill="white" transition="all" :selected="isOpen" rotate="on-selected:-180deg")
 
     Transition(name="slide-top")
-      Collapse(:when="isOpen" class="v-collapse")
+      Collapse(:when="isOpen" class="v-collapse"  overflow-y="auto" )
         slot
 
 </template>
