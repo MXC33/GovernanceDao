533dd93f9071aac20406df95e8fff3389d8a46e3
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-24 11:14:13 +0200

    Update row color

--- apps/marketplace-fullscreen/components/Attribute/Row.vue
+++ apps/marketplace-fullscreen/components/Attribute/Row.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div(grid="~ cols-3 gap-3" b="b-1 gray-400 last:b-0" capitalize="~" px="3" items="center" font="bold" color="gray-50" min-h="12")
+div(grid="~ cols-3 gap-3" b="b-1 gray-600 last:b-0" capitalize="~" px="3" items="center" font="bold" color="gray-50" min-h="12")
   div(grid="col-span-2")
     slot(name="name")
 
