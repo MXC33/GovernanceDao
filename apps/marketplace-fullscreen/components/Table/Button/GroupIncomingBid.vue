e185922f346273cd3d02c72d458cb0c83cd42b20
goerris <gustav.seffer@gmail.com>
2023-05-26 16:56:33 +0200

    add bid methods to click

--- apps/marketplace-fullscreen/components/Table/Button/GroupIncomingBid.vue
+++ apps/marketplace-fullscreen/components/Table/Button/GroupIncomingBid.vue
@@ -1,22 +1,24 @@
 <template lang="pug">
 HList(items="center" p-t="3" space-x="10")
-  ButtonSound(sound="sm" btn="~ secondary" cut="bottom-right lg" @click="counterBid") Counter  
-  ButtonSound(sound="sm" btn="~ primary" cut="bottom-right lg" @click="acceptBid") Accept
+  ButtonSound(sound="sm" btn="~ secondary" cut="bottom-right lg" @click="counterBidOnClick") Counter  
+  ButtonSound(sound="sm" btn="~ primary" cut="bottom-right lg" @click="acceptBidOnClick") Accept
 </template>
 
 <script setup lang="ts">
 import type { TableRow } from '~/composables/useTable';
 
 
+const { counterBid, acceptBid } = useBidsAPI()
+
 const props = defineProps<{
   row: TableRow,
 }>()
 
-const counterBid = () => {
+const counterBidOnClick = () => {
   console.log(props.row)
 }
 
-const acceptBid = () => {
+const acceptBidOnClick = () => {
   console.log(props.row)
 }
 
