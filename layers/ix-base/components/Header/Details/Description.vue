6881f2e537ace461cbec3586978b123a25a1f62f
Ceronfred <122262506+Ceronfred@users.noreply.github.com>
2023-05-26 11:07:53 +0200

    Update Description.vue

--- layers/ix-base/components/Header/Details/Description.vue
+++ layers/ix-base/components/Header/Details/Description.vue
@@ -1,18 +1,22 @@
 <template lang="pug">
 div()
-  HList(p ="3" color="hover:red")
+  HList(:class="{ 'hovered': isHovered }" @mouseover="isHovered = true" @mouseleave="isHovered = false" p="3" :color="isHovered ? 'red' : ''")
+    TrashIcon(w="10" h="10" p="1")
     VList()
       div() {{title}} 
-      div(color="gray") {{description}}
+      div(:style="{ color: isHovered ? 'red' : 'gray' }") {{ description }}
 </template>
 <script lang="ts" setup>
-
+import TrashIcon from '~/assets/icons/trash.svg'
 const props = defineProps<{
   title: string,
   description: string,
+  isHovered?: boolean
 }>()
-
-
-// @click="$emit(title)"
-//defineEmits(['clicked'])
+const isHovered = ref(false)
 </script>
+<style scoped>
+.hovered {
+  color: red;
+}
+</style>
