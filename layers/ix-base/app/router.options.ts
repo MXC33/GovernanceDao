4f25287d9a34fe755fa051513de85a5e3763857b
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-05-26 13:27:21 +0200

    Remove some logs

--- layers/ix-base/app/router.options.ts
+++ layers/ix-base/app/router.options.ts
@@ -2,21 +2,17 @@ import type { RouterOptions } from '@nuxt/schema'
 
 export default <RouterOptions>{
   scrollBehavior(to, from, savedPosition) {
-    console.log('scroll behavior function fired');
-    const nuxtApp = useNuxtApp();
+    const nuxtApp = useNuxtApp()
 
     return new Promise((resolve) => {
       nuxtApp.hook('page:finish', () => {
-        console.log('page loaded');
 
-        if (savedPosition) {
+        if (savedPosition)
           return resolve(savedPosition)
-        }
-
         resolve({
           top: 0,
-        });
-      });
-    });
-  },
+        })
+      })
+    })
+  }
 }
