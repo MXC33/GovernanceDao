c4ee9e10f4c97a2a99c391b26d2a026e82331d15
Philip Wallner <philipwallner@outlook.com>
2023-05-26 11:28:03 +0200

    Color change on hover fixed

--- layers/ix-base/components/Header/Details/Description.vue
+++ layers/ix-base/components/Header/Details/Description.vue
@@ -1,22 +1,18 @@
 <template lang="pug">
 div()
-  HList(:class="{ 'hovered': isHovered }" @mouseover="isHovered = true" @mouseleave="isHovered = false" p="3" :color="isHovered ? 'red' : ''")
+  HList(group p="3")
     TrashIcon(w="10" h="10" p="1")
-    VList()
-      div() {{title}} 
-      div(:style="{ color: isHovered ? 'red' : 'gray' }") {{ description }}
+    VList( )
+      div(color="white group-hover:red" ) {{title}} 
+      div(color="gray-200 group-hover:red" ) {{ description }}
 </template>
+
 <script lang="ts" setup>
 import TrashIcon from '~/assets/icons/trash.svg'
+
 const props = defineProps<{
   title: string,
   description: string,
-  isHovered?: boolean
 }>()
-const isHovered = ref(false)
+
 </script>
-<style scoped>
-.hovered {
-  color: red;
-}
-</style>
