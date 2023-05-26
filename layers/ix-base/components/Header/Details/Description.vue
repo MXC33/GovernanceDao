e7322331b6aab9377aea72751abfa7368a05ca53
Philip Wallner <philipwallner@outlook.com>
2023-05-26 14:18:23 +0200

    Revert "Revert "Updated to correct colors and hover image""
    This reverts commit 51adf07071b1e7537230f0b9e76251f5e4a8968a.

--- layers/ix-base/components/Header/Details/Description.vue
+++ layers/ix-base/components/Header/Details/Description.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 div()
   HList(group p="3")
-    TrashIcon(w="10" h="10" p="1")
+    TrashIcon(w="10" h="10" p="1" fill="white group-hover:$mc-orange")
     VList( )
-      div(color="white group-hover:red" ) {{title}} 
-      div(color="gray-200 group-hover:red" ) {{ description }}
+      div(color="white group-hover:$mc-orange" text="16px" ) {{title}} 
+      div(color="gray-200 group-hover:$mc-orange" ) {{ description }}
 </template>
 
 <script lang="ts" setup>
