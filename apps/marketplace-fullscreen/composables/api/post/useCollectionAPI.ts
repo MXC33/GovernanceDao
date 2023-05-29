8203a577bd95cb1d516212662699e261e6b67cad
goerris <gustav.seffer@gmail.com>
2023-05-29 14:56:28 +0200

    first commit

--- apps/marketplace-fullscreen/composables/api/post/useCollectionAPI.ts
+++ apps/marketplace-fullscreen/composables/api/post/useCollectionAPI.ts
@@ -14,8 +14,42 @@ interface CollectionsResponse {
 }
 
 
-export const useCollectionData = (slug: string, body: ComputedRef<CollectionPayload>, network = 'polygon') => {
-  return useAsyncDataState('collection-' + slug + body.value.page_key, () =>
+export const useCollectionData = (slug: string, network = 'polygon') => {
+  const { activeFilters, filtersAsPayload } = useCollectionSettings()
+  const activePage = ref(0)
+
+  const body = computed<CollectionPayload>(() => ({
+    page_key: activePage.value,
+    order: 0,
+    filter: {
+      owned: false,
+      type: 0,
+      search: "",
+      attributes: filtersAsPayload.value
+    }
+  }))
+
+  const setupCollectionListeners = (data: Ref<CollectionData>, refresh: () => Promise<void>) => {
+    watch(activeFilters, () => {
+      activePage.value = 0
+      refresh()
+      console.log("New filter")
+    }, { deep: true })
+
+    watch(data, (value) => {
+      if (!value)
+        return
+
+      if (activeFilters.value.length < 1)
+        createFilters(value)
+
+      console.log("NEw data", value.nfts.length)
+
+    }, { deep: true, immediate: true })
+  }
+
+
+  const asyncState = useAsyncDataState('collection-' + slug + body.value.page_key, () =>
     fetchIXAPI('collections/' + network + '/' + slug, 'POST', body.value) as Promise<CollectionResponse>, {
     transform: (item) => {
       console.log("Transformed the data", body.value)
@@ -28,6 +62,10 @@ export const useCollectionData = (slug: string, body: ComputedRef<CollectionPayl
       }
     }
   })
+
+  return {
+    ...asyncState,
+  }
 }
 
 export const useCollectionsData = (network = 'polygon') =>
--- apps/marketplace-fullscreen/pages/assets/[network]/[contract]/index.vue
+++ apps/marketplace-fullscreen/pages/assets/[network]/[contract]/index.vue
@@ -9,25 +9,12 @@ button(@click="loadMore") LoadMore
 import { type FilterPayload, useCollectionSettings } from "~/composables/useCollection";
 import type { CollectionPayload } from '~/composables/useCollection';
 
-const { activeFilters, createFilters, filtersAsPayload } = useCollectionSettings()
+const { activePage, collectionBody, setupCollectionListeners } = useCollectionSettings()
 
 const route = useRoute()
 const { contract } = route.params
 
-const activePage = ref(0)
-
-const body = computed<CollectionPayload>(() => ({
-  page_key: activePage.value,
-  order: 0,
-  filter: {
-    owned: false,
-    type: 0,
-    search: "",
-    attributes: filtersAsPayload.value
-  }
-}))
-
-const { data: data, execute: fetchCollection, fetchAndMerge: fetchNewPage, refresh: refresh } = useCollectionData(String(contract), body)
+const { data: data, execute: fetchCollection, fetchAndMerge: fetchNewPage, refresh: refresh } = useCollectionData(String(contract), collectionBody)
 
 const loadMore = () => {
   activePage.value = Number(data.value?.page_key)
@@ -36,25 +23,5 @@ const loadMore = () => {
 
 await fetchCollection()
 
-watch(activeFilters, () => {
-  activePage.value = 0
-  refresh()
-  console.log("New filter")
-}, { deep: true })
-
-watch(data, (value) => {
-  if (!value)
-    return
-
-  if (activeFilters.value.length < 1)
-    createFilters(value)
-
-  console.log("NEw data", value.nfts.length)
-
-}, { deep: true, immediate: true })
-
-onBeforeUnmount(() => {
-  activeFilters.value = []
-})
 
 </script>
