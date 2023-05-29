9031636beb3cd203919c85abe36b05d5a35c81ca
Philip Wallner <philipwallner@outlook.com>
2023-05-29 12:41:13 +0200

    Test Icon added

--- layers/ix-base/components/Header/Details/Description.vue
+++ layers/ix-base/components/Header/Details/Description.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
 div()
   HList(group p="3")
-    TitleIcon(w="10" h="10" p="1" fill="white group-hover:$mc-orange")
+    //-TitleIcon(w="10" h="10" p="1" fill="white group-hover:$mc-orange")
+    HeaderDetailsIcon(:title="subHeaderObject.title")
     VList( )
       div(color="white group-hover:$mc-orange" text="16px" ) {{subHeaderObject.title}} 
       div(color="gray-200 group-hover:$mc-orange" ) {{ subHeaderObject.description }}
---
+++ layers/ix-base/components/Header/Details/Icon.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+ixtmaticIcon(w="10" h="10" p="1" fill="white group-hover:$mc-orange" v-if="title == 'IXT/MATIC (Sushiswap)'")
+ixtusdtIcon(w="10" h="10" p="1" fill="white group-hover:$mc-orange" v-if="title == 'IXT/USDT (Quickswap)'")
+lendCapitalIcon(w="10" h="10" p="1" fill="white group-hover:$mc-orange" v-if="title == 'LEND CAPITAL'")
+polygonIcon(w="10" h="10" p="1" fill="white group-hover:$mc-orange" v-if="title == 'Polygon'")
+swapIcon(w="10" h="10" p="1" fill="white group-hover:$mc-orange" v-if="title == 'Swap'")
+</template>
+
+<script lang="ts" setup>
+import ixtmaticIcon from '~/assets/images/icons/dropdown/ixtmatic.svg'
+import ixtusdtIcon from '~/assets/images/icons/dropdown/ixtusdt.svg'
+import lendCapitalIcon from '~/assets/images/icons/dropdown/lend-capital.svg'
+import polygonIcon from '~/assets/images/icons/dropdown/polygon.svg'
+import swapIcon from '~/assets/images/icons/dropdown/swap.svg'
+
+const props = defineProps<{
+  title: string
+}>()
+
+</script>
