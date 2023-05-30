c0c2930e2874ab9b3818803ad1893e387ae9fb39
Philip Wallner <philipwallner@outlook.com>
2023-05-30 10:58:37 +0200

    svg file restructior

--- layers/ix-base/components/Header/Category/Index.vue
+++ layers/ix-base/components/Header/Category/Index.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-VList() 
+div()
   div(text="ix-orange" p="2") {{ $t(`marketplace.headers.${parentType}.${category.type}.title`) }} 
   div(b="b-1 ix-orange" w="95%")
 
-  div(grid="~ cols-3")
+  div(grid="~ cols-1 s-two:cols-2 s-three:cols-3" :state="gridState")
     HeaderCategoryItem(:header="parentType" :category="category.type" :item="item" v-for="item in category.items" @click="$emit('onClickItem', item)") 
 
 </template>
@@ -15,4 +15,15 @@ const props = defineProps<{
   parentType: string,
   category: HeaderCategory
 }>()
+
+
+const gridState = computed(() => {
+  const gridItems = props.category.items.length
+  if(gridItems > 6)
+    return 'three'
+  
+  if(gridItems > 3)
+    return 'two'
+})
+
 </script>
