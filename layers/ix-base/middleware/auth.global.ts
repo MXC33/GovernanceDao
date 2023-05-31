6d6ca2ad2f5cc332c8d046202d065e95dd86ce7f
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-05-31 16:40:02 +0200

    middleware for "/"

--- layers/ix-base/middleware/auth.global.ts
+++ layers/ix-base/middleware/auth.global.ts
@@ -2,6 +2,9 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   if (to.path == '/connect')
     return true
 
+  if (to.path == '/')
+    return navigateTo('/assets/polygon/0xba6666b118f8303f990f3519df07e160227cce87')
+
   const { isWalletConnected, logoutWallet } = useWallet()
   const redirect = useLoginRedirect()
   const { user, removeUser } = useUser()
