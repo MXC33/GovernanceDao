3089867bf05e1c9743c0a5f473cf68ba43276ce2
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-06-01 12:23:22 +0200

    Update for trademodule when owning 721

--- apps/marketplace-fullscreen/components/TradeModule/Index.vue
+++ apps/marketplace-fullscreen/components/TradeModule/Index.vue
@@ -5,10 +5,12 @@ VList(w="full" bg="gray-900")
       template(v-for="tab in tabs")
         TabItem(:id="tab" v-model="activeTab") {{ tab }}
 
-    span(color="white" px="6") You own: {{ item.my_shares }}
+    span(color="white" px="6" v-if="!is721") You own: {{ item.my_shares }}
+    span(color="white" px="6" v-else-if="is721 && canSell") Owned by: You
+
 
   Transition(name="fade-slow" mode="out-in")
-    TradeModuleSell(v-if="activeTab == 'sell' && item.my_shares > 0" :item="item")
+    TradeModuleSell(v-if="activeTab == 'sell' && item.my_shares > 0" :item="item" :is-disabled="item.bids.length == 0")
     TradeModuleBuy(v-else :item="item")
 
 </template>
@@ -19,8 +21,21 @@ import type { SingleItemData } from '@ix/base/composables/Token/useIXToken';
 const { item } = defineProps<{
   item: SingleItemData
 }>()
+
+const is721 = ERC721Addresses.includes(item.collection)
+
+const cantBuy = computed(() => item.my_shares > 0 && is721)
 const canSell = computed(() => item.my_shares > 0)
 
-const { tabs, activeTab } = useTabList(['sell', 'buy'].filter((tab) => tab == 'buy' || canSell.value))
+console.log(cantBuy.value, 'cant buy')
 
+// const { tabs, activeTab } = useTabList(['sell', 'buy'].filter((tab) => tab == 'buy' || cantBuy.value || canSell.value))
+
+const { tabs, activeTab } = useTabList(['sell', 'buy'].filter((tab) => {
+  if (tab === 'buy') {
+    return !cantBuy.value
+  } else {
+    return cantBuy.value || canSell.value
+  }
+}))
 </script>
--- apps/marketplace-fullscreen/components/TradeModule/Sell.vue
+++ apps/marketplace-fullscreen/components/TradeModule/Sell.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
 VList()
   VList(px="6" py="8" space-y="2")
-    span(color="gray-200") Total Price
+    span(color="gray-200") Best offer
 
     HList(items="end" space-x="3")
-      span(color="white" font="bold" text="4xl") {{ item.sale_price }} IXT
-      span(color="gray-200" font="bold" text="lg") ${{ ixtToUSD(item.sale_price) }}
+      span(color="white" font="bold" text="4xl" v-if="!isDisabled") {{ item.sale_price }} IXT
+      span(color="white" font="bold" text="4xl" v-else="isDisabled") -- IXT
+      span(color="gray-200" font="bold" text="lg" v-if="!isDisabled") ${{ ixtToUSD(item.sale_price) }}
+      span(color="gray-200" font="bold" text="lg" v-if="isDisabled") $ --
+
 
   //- SAVE BELOW FOR FUTURE NEED
   //- HList(px="6" py="3.5" b="t-1 b-1 gray-600" space-x="3" items="center")
@@ -13,9 +16,10 @@ VList()
   //-   span(color="gray-200") Max price per listing
 
   div(grid="~ cols-2" text="base")
-    button(btn="~ secondary" font="bold") Accept offer
-    ButtonInteractive(btn="~ primary" font="bold" @click="onClickSell" text="List item")
+    button(btn="~ secondary on-disabled:green" font="bold" v-if="!isDisabled") Accept offer
+    button(btn="~ secondary" bg="on-disabled:gray-700" color="on-disabled:gray-400" cursor="default" font="bold" :disabled="isDisabled" v-else) You have no offers
 
+    ButtonInteractive(btn="~ primary" font="bold" @click="onClickSell" text="List item")
 
 </template>
 
@@ -33,8 +37,9 @@ const onClickSell = () => {
   })
 }
 
-const { item } = defineProps<{
+const { item, isDisabled } = defineProps<{
   item: IXToken
+  isDisabled?: boolean
 }>()
 
 </script>
