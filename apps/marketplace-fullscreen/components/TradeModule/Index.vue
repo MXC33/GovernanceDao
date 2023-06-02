58c8f2aee46cc04063498a03dbc5e524d45b72cd
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-06-02 17:04:36 +0200

    Still placeholder UI for mobile on SingleItem

--- apps/marketplace-fullscreen/components/TradeModule/Index.vue
+++ apps/marketplace-fullscreen/components/TradeModule/Index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 VList(w="full" bg="gray-900")
-  HList(w="full" justify="between" items="center" b="b-1 gray-600")
+  HList(w="full" justify="between" items="center" b="b-1 gray-600" text="lt-md:xs" font="lt-md:bold")
     HList()
       template(v-for="tab in tabs")
         TabItem(:id="tab" v-model="activeTab") {{ tab }}
