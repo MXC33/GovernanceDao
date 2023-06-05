3a23b92217af5e939d1473b7737d7f3f24b77f6f
Ceronfred <122262506+Ceronfred@users.noreply.github.com>
2023-06-05 11:48:24 +0200

    Update Index.vue
    refactored some code

--- layers/ix-base/components/Header/AccountButton/Index.vue
+++ layers/ix-base/components/Header/AccountButton/Index.vue
@@ -8,14 +8,14 @@ VList(pos="relative"  ref="menuElement")
       div(grow="~") 
   VList(v-if="menuOpen" pos="absolute top-full left-0 right-0" b="1 gray-400" items="left" z="99") 
     HeaderLink(to="")
-      div(h="10" p="3" bg="black" b="b-1 gray-600" text="left" style="text-transform: none;" @click="addFunds" color="hover:$mc-orange") {{ $t(`marketplace.navigation.menu.addFunds`)}}
+      div(v-bind="commonAttrs" @click="addFunds") {{ $t(`marketplace.navigation.menu.addFunds`)}}
     HeaderLink(group to="")
-      div(h="10" p="3" bg="black" b="b-1 gray-600" text="left" style="text-transform: none;" @click="copyAddressToClipboard" color="white group-hover:$mc-orange") {{formatAdress}}
+      div(v-bind="commonAttrs" @click="copyAddressToClipboard") {{formatAdress}}
         Copy(w="6" pos="relative" bottom="25px" left="125px" fill="white group-hover:$mc-orange")
     HeaderLink(to="/account")
-      div(h="10" p="3" bg="black" b="b-1 gray-600" text="left" style="text-transform: none;" color="white group-hover:$mc-orange" @click="toggleMenu") {{ $t(`marketplace.navigation.menu.account`)}}
+      div(v-bind="commonAttrs" @click="toggleMenu") {{ $t(`marketplace.navigation.menu.account`)}}
     HeaderLink(to="/connect")
-      div(h="10" p="3" bg="black" b="b-1 gray-600" text="left" color="hover:$mc-orange" style="text-transform: none;" @click="toggleMenu") {{ $t(`marketplace.navigation.menu.logout`)}}
+      div(v-bind="commonAttrs" @click="toggleMenu") {{ $t(`marketplace.navigation.menu.logout`)}}
 </template>
     
     
@@ -61,5 +61,16 @@ const formatAdress = computed(() => {
   const lastFour = currentAdress.slice(-4);
   return `${firstFour}...${lastFour}`;
 })
+
+const commonAttrs = {
+  h: "10",
+  p: "3",
+  bg: "black",
+  b: "b-1 gray-600",
+  text: "left",
+  style: "text-transform: none;",
+  color: "white group-hover:$mc-orange"
+};
+
 
 </script>
