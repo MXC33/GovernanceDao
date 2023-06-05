117ae3ccbca82a813b412bf20388afc4ab9d1b02
Ceronfred <122262506+Ceronfred@users.noreply.github.com>
2023-06-05 11:42:59 +0200

    Update Account.vue
    hello

--- layers/ix-base/components/Header/Category/DropDown/Account.vue
+++ layers/ix-base/components/Header/Category/DropDown/Account.vue
@@ -2,22 +2,25 @@
 div(bg="black")
   HList(p="6")
     //-div(grow="~")
-    HList(grow="~")
+    NuxtLink(v-if="walletState !== 'connected'" bg="$mc-orange_20" h="48px" b="1 $mc-orange" color="$mc-orange" px="8" to="/connect" grow="~") 
+      div(text="center lg" p="2" font="bold") Connect Wallet 
+    HList(v-else grow="~")
       Icon(w="20" p="3" b="1 $mc-orange_40" bg="$mc-orange_20" )
       VList(opacity="100%" grow="~")
         div(p="2" b="1 $mc-orange_40" text="white" bg="$mc-orange_20" v-if="userId") {{userId}}
           NuxtLink(to="/account")
-            div(text="$mc-orange" font="") Account settings
+            div(text="$mc-orange" font="") My Assets
         HList()
           HList(b="1 $mc-orange_40" p="2" grow="~" bg="$mc-orange_20")
             div() IXT : 
             div(text="green") {{ Math.round((ixtBalance ?? 0) * 100) / 100 }}
           div(b="1 $mc-orange_60" p="1" cut="bottom-right" bg="$mc-orange_40") +  
-    //-div(grow="~")
+  //-div(grow="~")
 </template>
 
 <script lang="ts" setup>
 import FallbackVue from '~/components/Fallback.vue';
+const { walletState } = useWallet()
 const { ixtBalance, fetchIXT } = useIXTContract()
 const { user } = useUser()
 
