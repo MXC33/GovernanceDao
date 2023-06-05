d9649e00d5c1ddf4ccee6c5e2ab19b5e8c39eff9
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-06-05 16:37:28 +0200

    Update of navigation and removal of "copy address"

--- layers/ix-base/components/Header/Category/DropDown/Index.vue
+++ layers/ix-base/components/Header/Category/DropDown/Index.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
 VList(display="md:none" pos="fixed" inset="0" bg="black" overflow-y="auto" m="!l-0")
-  HList(items="center"  bg="gray-800" px="4 md:7.5" h="16")
+  HList(items="center"  bg="gray-800" px="4 md:7.5" h="16" space-x="3" justify="between")
     NuxtLink(to="https://www.planetix.com")
-      PlanetIXNew(w="42.25")  
-    div(grow="~")
-    HelperLanguage(language="EN")
-    CrossIcon(pos="right" w="8" display="md:none" @click="$emit('close')")
+      PlanetIXNew(w="42.25")
+
+    HList(space-x="4")
+      HelperLanguage(language="EN" font="bold")
+      CrossIcon(pos="right" w="8" display="md:none" @click="$emit('close')")
+
   HeaderCategoryDropDownAccount(@swap="Swap" @ConnectWallet="ConnectWallet")
   HeaderCategoryDropDownGrid()
   HeaderButtonDisconnect(@disconnect="$emit('disconnect')" pos="sticky bottom-0" z="1")
@@ -17,12 +19,12 @@ import CrossIcon from '~/assets/images/header/cross.svg'
 import PlanetIXNew from '~/assets/images/header/planetix-new.svg'
 const emit = defineEmits(['swap', 'ConnectWallet', 'close'])
 
-const ConnectWallet = () =>{
+const ConnectWallet = () => {
   console.log("index Connect Wallet");
   emit('ConnectWallet')
 }
 
-const Swap = () =>{
+const Swap = () => {
   console.log("index Swap");
   emit('swap')
 }
