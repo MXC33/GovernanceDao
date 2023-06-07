5fde358146584dcdb518743f2a23f48978d10d1b
Monkey D Luffy <luffy122205@gmail.com>
2023-06-07 14:29:06 +0200

    Load more button under my assets and HIDE button if is last page (page_key = null)

--- apps/marketplace-fullscreen/pages/account/index.vue
+++ apps/marketplace-fullscreen/pages/account/index.vue
@@ -2,14 +2,18 @@
 Collection(:data="data" v-if="data" :context="'my-assets'")
   template(#menu)
     AccountMenu()
+
+  template(#bottom)
+    HList(w="lt-md:full" justify="center" py="2" v-if="!!data.page_key && data.page_key > 0")
+      button(@click="loadNextPage" btn="~ primary" py="3" cut="bottom-right sm b-ix-primary" bg="hover:ix-ne" w="lt-md:full") Load More
 </template>
-  
-  
+
+
 <script lang="ts" setup>
 
 const { myAssetsURL } = useCollectionsURL()
 
-const { data: data, execute: fetchCollection, setupCollectionListeners } = useCollectionData(myAssetsURL('polygon'), {
+const { data: data, execute: fetchCollection, loadNextPage, setupCollectionListeners } = useCollectionData(myAssetsURL('polygon'), {
   filter: {
     owned: true,
     type: 0,
--- apps/marketplace-fullscreen/pages/assets/[network]/[contract]/index.vue
+++ apps/marketplace-fullscreen/pages/assets/[network]/[contract]/index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 Collection(:data="data" v-if="data")
   template(#bottom)
-    HList(w="lt-md:full" justify="center" py="2")
+    HList(w="lt-md:full" justify="center" py="2" v-if="!!data.page_key && data.page_key > 0")
       button(@click="loadNextPage" btn="~ primary" py="3" cut="bottom-right sm b-ix-primary" bg="hover:ix-ne" w="lt-md:full") Load More
 </template>
 
