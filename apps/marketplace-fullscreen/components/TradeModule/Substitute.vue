7f542041c9bbdc78a252e37fd18fdf7888e7a6a9
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-06-07 17:07:46 +0200

    height on substitute were not appreciated from creative

--- apps/marketplace-fullscreen/components/TradeModule/Substitute.vue
+++ apps/marketplace-fullscreen/components/TradeModule/Substitute.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
-HList(px="3 md:6" py="3 md:3" b="t-1 gray-600 md:(t-1 b-1 gray-600)" space-x="lt-md:3" items="center" justify="between" w="full" min-h="22 md:18")
+HList(px="3 md:6" py="3 md:3" b="t-1 gray-600 md:(t-1 b-1 gray-600)" space-x="lt-md:3" items="center" justify="between" w="full")
   VList(fold="md" gap="3" w="full" whitespace="nowrap")
     InputCheckbox(v-model="isSubstitue")
       span(color="gray-200") 
         slot(name="title")
 
-  Transition(name="slide-right")
+  Transition(name="slide-top")
     div(flex="~ col md:row" color="white" w="full" align="lt-md:left" justify="end" whitespace="nowrap" gap="1 md:3" items="start md:center" v-if="isSubstitue")
       slot(name="substituteTitle")
 
