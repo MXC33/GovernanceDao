c3c7f18d1f9e020017dfed514c70eedec597f414
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-06-08 17:02:58 +0200

    small fixes

--- apps/marketplace-fullscreen/components/Dropdown/CollectionMobile.client.vue
+++ apps/marketplace-fullscreen/components/Dropdown/CollectionMobile.client.vue
@@ -1,23 +1,25 @@
 <template lang="pug">
-VList(w="full" h="100vh" bg="gray-900" z="99" pos="sticky top-16 left-0" v-if="isOpen")
-  HList(justify="between" items="center" b="b-0.5 gray-600" px="4" py="3")
+VList(w="full" h="100vh" bg="gray-900" z="99" pos="sticky top-16 left-0" v-if="isOpen" overflow-x="hidden")
+  HList(justify="between" items="center" px="4" py="3")
     HList(items="center" space-x="2")
       CollectionIcon(w="6")
       span(color="white" font="bold" text="lg" translate-y="0.5" uppercase="~") explore collections
 
-    HList(items="center" translate-y="0.2")
-      CloseIcon(w="4" cursor="pointer" @click="$emit('close')" mr="2")
+    HList(items="center" translate-y="0.2" px="2")
+      CloseIcon(w="4" cursor="pointer" @click="$emit('close')")
 
   VList(w="full")
     template(v-for="item in collections")
-      HList(bg="hover:gray-700" mt="3" px="3" space-x="3")
-        NuxtLink(:to="'/assets/polygon/' + item.contract" flex="~ row" items="center" w="full" @click="$emit('close')")
-          div(w="10" h="10" mr="3")
-            img(src="~/assets/testfiles/test-collection-bg.png" )
+      HList(bg="hover:gray-700" justify="between" b="b-0.5 gray-600 first:t-0.5" px="3" py="1")
+        NuxtLink(:to="'/assets/polygon/' + item.contract" flex="~ row" flex-grow="1" items="center" @click="$emit('close')")
+          HList(w="10" h="10" flex-shrink="0")
+            img(src="~/assets/testfiles/test-collection-bg.png")
 
-          div(v-if="item.name != 'PlanetIX Assets'" flex-grow="1" font="bold") {{ item.name }}
-          div(v-else flex-grow="1" font="bold") PlanetIX - Assets
-          div(color="gray-200" font="normal" pr="3") {{ item.nft_count }}
+          HList(px="3" whitespace="nowrap" items="center" w="full" justify="between" translate-y="0.4" font="bold")
+            div(v-if="item.name != 'PlanetIX Assets'") {{ item.name }}
+            div(v-else) PlanetIX - Assets
+          HList(items="center" flex-grow="1" translate-y="0.4" px="4")
+            div(color="gray-200" font="normal") {{ item.nft_count }}
 
 </template>
 
