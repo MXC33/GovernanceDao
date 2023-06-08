6b01111517e9cf015d222067ea673d5457958ed5
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-06-08 16:27:56 +0200

    Close Icon for cart

--- apps/marketplace-fullscreen/components/Cart/Index.vue
+++ apps/marketplace-fullscreen/components/Cart/Index.vue
@@ -5,7 +5,10 @@ Transition(name="fade")
 Transition(name="slide-right")
   VList(pos="fixed right-0 top-0 bottom-0" overflow-y="auto" bg="ix-black" w="80 md:120" max-w="80%" z="200" color="white" ref="cartElement" v-if="viewingCart")
     header(pos="sticky top-0" px="6" space-y="3" bg="black" p="t-6 b-3" z="1")
-      h3(text="xl" font="bold") My Cart
+      HList(items="center" justify="between")
+        h3(text="xl" font="bold") My Cart
+        ClientOnly
+          CloseIcon(w="4" @click.stop="viewingCart = false" cursor="pointer" fill="white hover:opacity-80")
 
       div(b="b-0.5 gray-600" mx="-6")
 
@@ -26,16 +29,16 @@ Transition(name="slide-right")
   
   
 <script lang="ts" setup>
+import CloseIcon from '~/assets/icons/close.svg'
+
 const cartElement = ref()
 const { getTokenKey } = useTokens()
 const { viewingCart, cartItems, clearCart } = useCart()
-
 
 const onClearCart = () => {
   clearCart()
 }
 </script>
-  
 
 <style>
 .cart-move,
