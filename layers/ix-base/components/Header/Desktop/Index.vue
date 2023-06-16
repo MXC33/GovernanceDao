53c622ea9edcfc9819fd2ecffd9743aca74d24d9
Ceronfred <122262506+Ceronfred@users.noreply.github.com>
2023-06-16 16:01:02 +0200

    Changed text and added url links

--- layers/ix-base/components/Header/Desktop/Index.vue
+++ layers/ix-base/components/Header/Desktop/Index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-div(m="!l-0" p="6" grid="~ cols-5" b="1 red" w="full" 
-  bg="gray-800" pos="absolute top-full")
+div(m="!l-0" p="6" grid="~ cols-5" w="full" 
+  bg="black" pos="absolute top-full")
   HeaderCategory(grid="col-span-1" v-for="category in header.categories" items="center" :category="category" :parent-type="header.type" grow="" @onClickItem="onClickItem")
     
 </template> 
