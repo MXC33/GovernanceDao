47f373ffc5f0e2857ed1cdfa8b7ad5e41f029286
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-06-27 16:11:33 +0200

    Update for disconnect button

--- layers/ix-base/components/Wallet/Button/Disconnect.vue
+++ layers/ix-base/components/Wallet/Button/Disconnect.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-ButtonSound(sound="lg" btn="~ accent lg" color="white" text-shadow="none" @click="logoutWallet" cut="bottom-right") {{$t(`general.disconnect`)}}
+ButtonSound(sound="lg" btn="~ accent lg" color="white" text-shadow="none" @click="logoutWallet" cut="bottom-right s-md b-ix-primary") {{$t(`general.disconnect`)}}
 </template>
 
 <script setup lang="ts">
