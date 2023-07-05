3b16d842bd2c0f763e1be81f9195ab977f109174
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-07-05 11:27:25 +0200

    Quick fix for cart on mobile

--- apps/marketplace-fullscreen/components/Cart/Index.vue
+++ apps/marketplace-fullscreen/components/Cart/Index.vue
@@ -3,8 +3,8 @@ Transition(name="fade")
   div(pos="fixed" inset="0" z="199" v-if="viewingCart" bg="ix-black opacity-80" @click.stop="viewingCart = false")
 
 Transition(name="slide-right")
-  VList(pos="fixed right-0 top-0 bottom-0" overflow-y="auto" bg="ix-black" w="80 md:120" max-w="80%" z="200" color="white" ref="cartElement" v-if="viewingCart")
-    header(pos="sticky top-0" px="6" space-y="3" bg="black" p="t-6 b-3" z="3")
+  VList(pos="fixed right-0 top-0 bottom-0" overflow-y="auto" bg="ix-black" w="full md:120" max-w="md:80%" z="200" color="white" ref="cartElement" v-if="viewingCart")
+    header(pos="sticky top-0" px="6" space-y="3" bg="black" p="t-3 b-3 md:t-6 b-3" z="3")
       HList(items="center" justify="between")
         h3(text="xl" font="bold") {{ $t(`marketplace.cart.title`) }}
         CloseIcon(w="4" @click.stop="viewingCart = false" cursor="pointer" fill="white hover:opacity-80")
@@ -13,7 +13,7 @@ Transition(name="slide-right")
 
       HList(items="center" justify="between")
         h4(font="bold" text="md") {{ cartItems.length }} {{ $t(`marketplace.cart.items`) }}
-        button(font="bold" text="md" @click="onClearCart") {{ $t(`marketplace.cart.clearAll`) }}
+        button(font="bold" text="md" color="ix-ne" @click="onClearCart") {{ $t(`marketplace.cart.clearAll`) }}
 
     VList(flex-grow="1" space-y="3")
       TransitionGroup(name="cart")
