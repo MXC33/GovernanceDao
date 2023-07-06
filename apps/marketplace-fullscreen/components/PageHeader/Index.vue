c1ab046eb9d1e71e6248e121a9e6b5dad02f0bf2
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-07-06 10:59:35 +0200

    Change of background on account

--- apps/marketplace-fullscreen/components/PageHeader/Index.vue
+++ apps/marketplace-fullscreen/components/PageHeader/Index.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 VList(flex-grow="1" px="0 md:6" pos="relative")
-  .gradient-bg(pos="fixed left-0 right-0 top-12 md:top-16" h="50vh" :style="gradientStyle" v-if="!onSingleItem")
+  .gradient-bg(pos="fixed left-0 right-0 top-12 md:top-16" h="50vh" :style="gradientStyle" v-if="!onSingleItem && !onAccountPage")
+  .gradient-bg-account(pos="fixed left-0 right-0 top-12 md:top-16" h="50vh" :style="gradientStyle" v-if="!onSingleItem && onAccountPage")
 
 Transition(name="fade-slow")
   HList(w="full" items="center" justify="between" pos="sticky top-12 md:top-16" z="10" bg="on-locked:ix-black" space-x="3" p="x-4 y-4 md:(y-4 x-8)" translate-y="$header-offset" transition="all" :locked="isScrolling")
@@ -24,11 +25,15 @@ Transition(name="fade-slow")
 </template>
 
 <script lang="ts" setup>
+import type { CollectionContext } from '~/composables/useCollection';
 import NetEmpireLogo from '~/assets/icons/logo/netempire.svg'
-// import NetBetaLogo from '~/assets/icons/logo/netbeta.svg'
 import UserIcon from '~/assets/icons/user.svg'
 import CartIcon from '~/assets/icons/cart.svg'
 import GlobeIcon from '~/assets/icons/globe.svg'
+
+const { context } = defineProps<{
+  context?: CollectionContext
+}>()
 
 const { viewingCart } = useCart()
 const y = useGlobalWindowScroll()
@@ -47,6 +52,10 @@ const onSingleItem = computed(() =>
   route.matched.some((match) => match.path.includes("tokenId") || match.path.includes("connect"))
 )
 
+const onAccountPage = computed(() =>
+  route.matched.some((match) => match.path.includes('account'))
+)
+
 const gradientStyle = computed(() => ({
   opacity: 100 - (y.value / 500) * 100 + '%'
 }))
@@ -61,5 +70,10 @@ const isScrolling = computed(() => {
 .gradient-bg {
   background: rgb(12, 12, 12);
   background: linear-gradient(0deg, rgba(12, 12, 12, 0.5) 50%, rgba(203, 0, 16, 0.5) 100%);
+}
+
+.gradient-bg-account {
+  background: rgb(12, 12, 12);
+  background: linear-gradient(0deg, rgba(12, 12, 12, 0.5) 50%, rgba(99, 99, 99, 0.5) 100%);
 }
 </style>
