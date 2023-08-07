5cf6397091cc89c33b237a97f3613a61fa6ce4d5
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-08-07 11:07:09 +0200

    Fix space x issue for checkbox

--- apps/marketplace-fullscreen/components/Input/Checkbox.vue
+++ apps/marketplace-fullscreen/components/Input/Checkbox.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-HList(items="start" space-x="2 on-spacex:0" cursor="pointer" @click.stop="toggle" select="none" :spacex="spaceFalse")
+HList(items="start" space-x="2" cursor="pointer" @click.stop="toggle" select="none")
   HList(b="2 gray-400 on-selected:0" bg="on-selected:ix-primary" color="white" w="5" h="5" pos="relative" :selected="isSelected" flex-shrink="0")
     CheckmarkIcon(fill="white" w="3" h="3" pos="absolute-center" transition="all" v-if="isSelected")
 
-  label(cursor="pointer")
+  label(cursor="pointer" v-if="$slots.default")
     slot
 
 </template>
@@ -17,7 +17,4 @@ const toggle = () => {
   isSelected.value = !isSelected.value
 }
 
-const { spaceFalse } = defineProps<{
-  spaceFalse?: boolean
-}>()
 </script>
