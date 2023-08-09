eaecde770f13afccdf24b5708456658e8ecf1a81
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-08-09 16:58:41 +0200

    Updated handling of filters

--- apps/marketplace-fullscreen/components/Collection/Filter/Button/Container.client.vue
+++ apps/marketplace-fullscreen/components/Collection/Filter/Button/Container.client.vue
@@ -1,18 +1,17 @@
 <template lang="pug">
 Transition(name="slide-left")
   HList(v-if="numberOfSelected" flex-wrap="~" gap="3" p="l-4 on-active:(b-4 md:b-3)" w="md:full" bg="ix-black" whitespace="nowrap" :open="isOpen" :active="isFilterActive")
-    template(v-for="(item, filterIndex) in activeFilters")
+    template(v-for="(item, filterIndex) in selectedFilters")
       template(v-for="(option, index) in item.value")
-        CollectionFilterButton(v-model="activeFilters[filterIndex].value[index].selected" v-if="option.selected")
+        CollectionFilterButton(v-if="option.selected" @clear="clearFilter(filterIndex, index)")
           template(#category) {{ item.trait_type }}:
           template(#type) {{ option.name }}
 
-    CollectionFilterButtonClearAll(@click="clearFilters" v-if="moreThanOneSelected")
+    CollectionFilterButtonClearAll(@clearfilter="clearFilters" v-if="moreThanOneSelected")
 
 </template>
 
 <script lang="ts" setup>
-
 defineProps<{
   isOpen?: boolean
 }>()
@@ -31,11 +30,33 @@ const moreThanOneSelected = computed(() => {
 
 const isFilterActive = computed(() => activeFilters.value.length > 1)
 
+const selectedFilters = computed(() => {
+  return activeFilters.value
+    .map((filter, index) => {
+      const selectedOptions = filter.value
+        .map((option, optionIndex) => option.selected ? { ...option, originalIndex: optionIndex } : null)
+        .filter(option => option !== null) as { originalIndex: number, name: string, selected: boolean }[]
+
+      return {
+        originalIndex: index,
+        trait_type: filter.trait_type,
+        value: selectedOptions
+      }
+    })
+    .filter(filter => filter.value.length > 0)
+})
+
+const clearFilter = (filterIndex: number, optionIndex: number) => {
+  const originalFilterIndex = selectedFilters.value[filterIndex].originalIndex
+  const originalOptionIndex = selectedFilters.value[filterIndex].value[optionIndex].originalIndex
+  activeFilters.value[originalFilterIndex].value[originalOptionIndex].selected = false
+}
+
 const clearFilters = () => {
   activeFilters.value.forEach(filter => {
     filter.value.forEach(option => {
       option.selected = false
-    });
-  });
-};
+    })
+  })
+}
 </script>
