33f3a897a3e0ba4204ccc641be9f5562e9360fd1
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-08-22 14:45:54 +0200

    Update for FormatAmount

--- apps/marketplace-fullscreen/components/Collection/Grid/Item/Meta.vue
+++ apps/marketplace-fullscreen/components/Collection/Grid/Item/Meta.vue
@@ -4,7 +4,7 @@ VList(w="full" flex-grow="1" items="start" bg="gray-900" p="6" pos="relative")
     NuxtLink(:to="getItemLink(token)" flex="~ row" rel="noopener" items="center" space-x="3"  cursor="pointer")
       TokenName(:token="token" :key="getTokenKey(token)" text="base md:xl ellipsis" capitalize="~")
 
-      div(v-if="is1155" font="normal" text="sm md:base" color="gray-200") x{{showAssetAmount}}
+      div(v-if="is1155" font="normal" text="sm md:base" color="gray-200" whitespace="nowrap") x{{showAssetAmount}}
 
     div(text="sm md:lg") 
       slot(name="subtitle")
--- apps/marketplace-fullscreen/composables/useFormatNumber.ts
+++ apps/marketplace-fullscreen/composables/useFormatNumber.ts
@@ -1,11 +1,56 @@
+// export const useFormatNumber = () => {
+//   const formatAmount = (amount: number | string) => {
+//     if (typeof amount === 'string') {
+//       amount = parseFloat(amount)
+//     }
+
+//     if (amount >= 1000000) {
+//       const formattedAmount = (amount / 1000000).toFixed(1)
+//       return formattedAmount.replace(".0", "") + "M"
+//     }
+
+//     else if (amount >= 1000) {
+//       const formattedAmount = (amount / 1000).toFixed(1)
+//       return formattedAmount.replace(".0", "") + "K"
+//     }
+
+//     return amount.toString();
+//   }
+
+//   return {
+//     formatAmount
+//   }
+// }
+
 export const useFormatNumber = () => {
   const formatAmount = (amount: number | string) => {
     if (typeof amount === 'string') {
       amount = parseFloat(amount)
     }
-    if (amount > 1000)
-      return String(amount / 1000).substring(0, 4) + "K"
-    return amount
+
+    const formatNumber = (num: number, divisor: number, suffix: string) => {
+      let base = num / divisor
+      let formatted = String(base)
+      let dotIndex = formatted.indexOf('.')
+
+      if (dotIndex !== -1) {
+        formatted = formatted.substring(0, dotIndex + 3)
+        if (formatted.endsWith('.00')) {
+          formatted = formatted.substring(0, dotIndex)
+        }
+      }
+
+      return formatted + suffix
+    }
+
+    if (amount >= 1000000) {
+      return formatNumber(amount, 1000000, 'M')
+    }
+    else if (amount >= 1000) {
+      return formatNumber(amount, 1000, 'K')
+    }
+
+    return amount.toString()
   }
 
   return {
