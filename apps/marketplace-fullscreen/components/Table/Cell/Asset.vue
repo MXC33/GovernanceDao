d454a6bd6372b900a38807d0c73ed25e453f5841
19OZMAN <76961722+19ozman@users.noreply.github.com>
2023-08-23 09:32:09 +0200

    Fix for token my_shares on table

--- apps/marketplace-fullscreen/components/Table/Cell/Asset.vue
+++ apps/marketplace-fullscreen/components/Table/Cell/Asset.vue
@@ -7,7 +7,7 @@ HList(min-w="0" pos="relative")
     InputCheckbox(:model-value="isItemSelected(token)" @update:modelValue="() => toggleItem(token)" v-if="!column.disableSelect" flex-shrink="0")
 
     TableCellToken(:token="token" max-w="full" @click="onClickAsset" min-w="0" :hide-name="scrolling")
-      span(v-if="context == 'my-assets'" color="gray-200" font="normal" text="lt-md:sm") x{{ token.my_shares }}
+      span(v-if="onAccountPage" color="gray-200" font="normal" text="xs md:sm" translate-y="0.8") x{{ token.my_shares }}
 
   Transition(name="slide-right" mode="in-out")
     div(pos="absolute top--3 bottom--3 md:(top--2 bottom--2)" left="-8" bg="gray-900" w="31 md:40" v-if="scrolling")
@@ -18,11 +18,12 @@ HList(min-w="0" pos="relative")
 </template>
 
 <script setup lang="ts">
-import type { TableColumnAsset } from '~/composables/useTable';
-import type { CollectionContext } from '~/composables/useCollection';
-import type { IXToken } from '@ix/base/composables/Token/useIXToken';
-const { isItemSelected, toggleItem } = useSelection()
+import type { TableColumnAsset } from '~/composables/useTable'
+import type { CollectionContext } from '~/composables/useCollection'
+import type { IXToken } from '@ix/base/composables/Token/useIXToken'
 
+const { isItemSelected, toggleItem } = useSelection()
+const route = useRoute()
 
 const { column, context, token, scrolling } = defineProps<{
   column: TableColumnAsset,
@@ -42,6 +43,9 @@ const tableScroll = computed(() => {
   return ''
 })
 
+const onAccountPage = computed(() =>
+  route.matched.some((match) => match.path.includes('account'))
+)
 </script>
 
 <style>
