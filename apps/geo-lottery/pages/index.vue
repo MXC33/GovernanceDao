b01a88988ad3be9b5a4067393b23d5781616327c
lony <lonybrd2590@gmail.com>
2023-09-15 10:14:30 +0200

    change bg size, margin button in purchase ticket

--- apps/geo-lottery/pages/index.vue
+++ apps/geo-lottery/pages/index.vue
@@ -4,7 +4,7 @@ div(class="body-main")
   VList(class="background-holder"  pos="relative" overflow="hidden" z="0")
     Background()
       template(#image-source)
-        img(src="~/assets/images/points.png" inset="0"  z="-1" object="contain top" h="full" w="full" pos="absolute")
+        img(src="~/assets/images/points.png" inset="0"  z="-1" object="contain top" h="auto" w="full" pos="absolute")
     div(w="full" flex="~" justify="center")
       div(container mx-3 lg:px-10 xl:px-30 space-y-14 md:space-y-18 lg:space-y-32 m="y-14 sm:y-22 md:y-32")
         //How to Play Section
