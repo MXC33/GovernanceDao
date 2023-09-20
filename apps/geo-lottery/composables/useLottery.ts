ab015eb94375dd8abbb6f86910280c4eced107d0
Alan Philip <alanphilipdev@gmail.com>
2023-09-20 11:05:22 +0200

    push live date

--- apps/geo-lottery/composables/useLottery.ts
+++ apps/geo-lottery/composables/useLottery.ts
@@ -16,7 +16,7 @@ export const useLottery = () => {
   } = useLuckyCatGeoLotteryContract()
 
   //const lotteryStartDate = useState<Date>('lottery-start-date', () => new Date(1694782800000))
-  const lotteryStartDate = useState<Date>('lottery-start-date', () => new Date(1695204000000))
+  const lotteryStartDate = useState<Date>('lottery-start-date', () => new Date(1695214800000))
   const isLotteryActive = useState<boolean>('lottery-active', () => false)
   const checkLotteryActive = () => {
     const now = new Date()
