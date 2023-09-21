0c56dc0755c6389025b6dc76ac7ae0b091e44c37
Rory Moore <rorymoore27@gmail.com>
2023-09-21 17:34:49 +0200

    Added territory lottery

--- apps/marketplace-fullscreen/components/Collection/Header.vue
+++ apps/marketplace-fullscreen/components/Collection/Header.vue
@@ -1,28 +1,38 @@
 <template lang="pug">
-VList(space-y="4 md:8")
+VList()
 
-  div(w="30 md:60" frame="~")
+  div(w="30 md:60" frame="~" space-y="4 md:8")
     slot(name="collectionImage")
       img(src="~/assets/images/collection/general.png" v-if="context == 'collection'")
       img(src="~/assets/images/collection/my-assets.png" v-else)
 
-  HList(items="center" space-x="2")
+  div(pos="absolute right-0 top-0" m="t-0" v-if="!isClosed")
+    a(href="https://territorylottery.planetix.com/")
+      img(src="~/assets/images/collection/territory-win.png" )
+    Close(pos="absolute right-4 top-10" w="3" cursor="pointer" @click="isClosed = true")
+
+
+  HList(items="center" space-x="2" space-y="4 md:8")
     span(color="gray-50" font="bold" text="3xl")
       slot(name="header")
 
     slot(name="cert")
 
-  HList(w="full")
+  HList(w="full" space-y="4 md:8")
     slot(name="attributes")
 
 </template>
 
 <script lang="ts" setup>
 import type { CollectionContext } from '~/composables/useCollection';
+import Close from '~/assets/icons/close.svg'
+
 
 defineProps<{
-  context: CollectionContext
+  closeTerritory: boolean
 }>()
+const isClosed = ref(false)
+
 </script>
 
 <style></style>
