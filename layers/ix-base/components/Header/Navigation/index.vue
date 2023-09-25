a6b6a524136c8c4d25964e5e64685eae419742be
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-09-25 09:39:03 +0200

    Remove old menu

--- layers/ix-base/components/Header/Navigation/index.vue
+++ layers/ix-base/components/Header/Navigation/index.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
 //-Display this when in portrait mode
-VList(v-if="isMobile" pos="fixed left-0 right-0 top-12" bg="black" h="[calc(100vh-var(--header-height))]" overflow-y="auto" z="200")
+VList(v-if="isMobile" pos="absolute left-0 right-0 top-$header-height" bg="black" h="[calc(100vh-var(--header-height))]" overflow-y="auto" z="99")
   HeaderNavigationAccount(@close="$emit('close')")
   HeaderNavigationMenu()
   HeaderNavigationButtonDisconnect(pos="sticky bottom-0" z="1")
+
 //-Display this when in landscape mode
-div(m="!l-0" p="6 t-3" grid="~ cols-5" w="full" bg="black" pos="absolute top-16 right-0 left-0" z="99" v-else)
+div(m="!l-0" p="6 t-3" grid="~ cols-5" w="full" bg="black" pos="absolute top-full right-0 left-0" z="99" v-else)
   HeaderNavigationMenuSubNavigation(grid="col-span-1 s-increased:col-span-2" v-for="(navigation, index) in header.subNavigations" items="center" grow="" :navigation="navigation" :state="columnSize[index]")
 </template>
 
