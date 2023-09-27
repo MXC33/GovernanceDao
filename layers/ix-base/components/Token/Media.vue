f1d9a87a9b6e82c51e72e368ff69996ac2e0e3e6
goerris <gustav.seffer@gmail.com>
2023-09-27 18:28:15 +0200

    Add IFrame to TokenMedia

--- layers/ix-base/components/Token/Media.vue
+++ layers/ix-base/components/Token/Media.vue
@@ -1,10 +1,13 @@
 <template lang="pug">
-TokenImage(v-if="displayStillImage" :token="token" :is-large="(!icon && isLarge) || image" :key="componentKey")
+TokenIFrame(v-if="contract==geoLotteryAddress" :token="token")
+TokenImage(v-else-if="displayStillImage" :token="token" :is-large="(!icon && isLarge) || image" :key="componentKey")
 TokenVideo(v-else :token="token" :is-large="isLarge" h="full" inset="0" :key="componentKey + 'video'" :fill="contain ? 'contain' : 'cover'")
 </template>
 
 <script lang="ts" setup>
 import type { AnyToken } from '~/composables/Token/useTokens';
+
+const { getChain } = useWallet()
 const props = defineProps<{
   token: AnyToken,
   isLarge?: boolean,
@@ -21,6 +24,11 @@ const getTokenAttribute = (item: AnyToken, attribute: string) => {
 
   return item.tokenInfo?.type
 }
+
+const geoLotteryAddress = geoLotteryRewardAddress[getChain('polygon')]?.toLowerCase()
+
+const contract = computed(() => props.token?.contract ?? props.token?.collection)
+
 const imageTokens = ['pix', 'anniversary-pack', 'loot-crate', 'starter-pack', 'coinbase', 'gg', 'arcade', 'tile-contract', 'cargo-drop', 'avatar', 'aoc-badge', 'voucher', 'gg2', 'rover-key', 'gg-energy', 'gg2-energy', 'gg3-energy', 'gg4-energy', 'landmark']
 
 const displayStillImage = computed(() => {
