4364b29e81c3488ccfce771e519207fafcee895a
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-09-27 16:06:12 +0200

    Fix stills for tokenMedia

--- layers/ix-base/components/Token/Media.vue
+++ layers/ix-base/components/Token/Media.vue
@@ -5,7 +5,6 @@ TokenVideo(v-else :token="token" :is-large="isLarge" h="full" inset="0" :key="co
 
 <script lang="ts" setup>
 import type { AnyToken } from '~/composables/Token/useTokens';
-
 const props = defineProps<{
   token: AnyToken,
   isLarge?: boolean,
@@ -14,9 +13,24 @@ const props = defineProps<{
   image?: boolean
 }>()
 
-// const imageTokens = ['pix', 'anniversary-pack', 'loot-crate', 'starter-pack', 'coinbase', 'gg', 'arcade', 'tile-contract', 'cargo-drop', 'avatar', 'aoc-badge', 'voucher', 'gg2', 'rover-key', 'gg-energy', 'gg2-energy', 'gg3-energy', 'gg4-energy', 'landmark']
+console.log("TOKEN", props.token)
 
-const displayStillImage = computed(() => !tokenHasVideo(props.token))
+
+const getTokenAttribute = (item: AnyToken, attribute: string) => {
+  if (isIXToken(item))
+    return item?.attributes?.find((attr) =>
+      attr.key?.toLowerCase() == attribute.toLowerCase()
+    )?.value_search
+
+  return item.tokenInfo?.type
+}
+const imageTokens = ['pix', 'anniversary-pack', 'loot-crate', 'starter-pack', 'coinbase', 'gg', 'arcade', 'tile-contract', 'cargo-drop', 'avatar', 'aoc-badge', 'voucher', 'gg2', 'rover-key', 'gg-energy', 'gg2-energy', 'gg3-energy', 'gg4-energy', 'landmark']
+
+const displayStillImage = computed(() => {
+  const tokenType = getTokenAttribute(props.token, 'type') ?? ""
+
+  return imageTokens.includes(tokenType) || !tokenHasVideo(props.token)
+})
 
 const componentKey = computed(() => props.token && getTokenKey(props.token))
 </script>
