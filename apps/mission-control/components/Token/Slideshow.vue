58687f77b4fc308243c092f6bd4faabc7ee014ae
goerris <gustav.seffer@gmail.com>
2023-09-28 11:49:50 +0200

    fix video being contained in corporation slideshow

--- apps/mission-control/components/Token/Slideshow.vue
+++ apps/mission-control/components/Token/Slideshow.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 Carousel(:items-to-show="tokens.length == 1 ? 0 : 1" :autoplay="2000" :wrap-around="true")
   Slide(v-for="(token, index) in tokens" :key="index")
-    TokenMedia(:token="token" :key="getNftTokenKey(token, '-', true)" h="full" w="full" pos="absolute" :icon="icon" :image="image")
+    TokenMedia(:token="token" :key="getNftTokenKey(token, '-', true)" h="full" w="full" pos="absolute" :icon="icon" :image="image" :contain="true")
   
     </template>
       
