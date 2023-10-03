b25fdb5e95f6553a11b4c43c789261328d1b9374
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-10-03 13:17:53 +0200

    Create useIXTPrice.ts

---
+++ layers/ix-base/composables/Utils/useIXTPrice.ts
@@ -0,0 +1,31 @@
+interface PriceResponse {
+  price: number
+}
+
+export const useIXTPrice = () => {
+  const config = useRuntimeConfig().public
+
+  const { data: ixtPrice, refresh: refreshIXTPrice, execute: fetchPrice } = useAsyncDataState('ixt-price', () =>
+    $fetch(config.MC_API + '/ixt-price', { mode: 'cors' }) as Promise<PriceResponse>, {
+    transform: (data) => Number(data)
+  })
+
+  const setupIXTPrice = () => {
+    useIntervalFn(() => { refreshIXTPrice() }, 1000 * 60)
+    return fetchPrice()
+  }
+
+  const ixtToUSD = (ixt: number) =>
+    roundToDecimals(Number(ixt) * (ixtPrice?.value ?? 0), 5)
+
+  const usdToIXT = (usd: number) =>
+    roundToDecimals(Number(usd) / (ixtPrice?.value ?? 0), 5)
+
+  return {
+    ixtPrice,
+    ixtToUSD,
+    usdToIXT,
+    setupIXTPrice
+  }
+
+}
