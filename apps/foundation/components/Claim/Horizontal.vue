b9e835a30c0466f45595687461ba800b0cfbbfed
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-09 15:46:12 +0200

    Added Horizontal claim component
    Got a weird bug where the components renders more than once.

---
+++ apps/foundation/components/Claim/Horizontal.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+HList()
+  h4(text="3xl") Claim rewards
+  p Rewards: [dynamic value]
+  button(btn="secondary") CLAIM REWARDS
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- apps/foundation/pages/landmarks/index.vue
+++ apps/foundation/pages/landmarks/index.vue
@@ -1,15 +1,11 @@
 <template lang="pug">
-VList(b="solid blue 1")
-
-
-  div(grid="~ cols-4 gap-6")
-    HeaderTitle(:index="13")
-
-  Card()
-  HeaderTitle(:index="14")
-  Card()
+VList(grid="~ cols-4 gap-6" b="solid purple 1")
+  HeaderTitle(:index="13" b="solid white 1")
+  ClaimHorizontal(b="solid green 1")
+  HeaderTitle(:index="14" b="solid red 1")
+  Card(b="solid blue 1")
     h3 No landmarks available
-  HeaderTitle(:index="15")
+  HeaderTitle(:index="15" b="solid yellow 1")
 
 </template>
 
