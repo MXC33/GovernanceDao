af2e8724cea4ef897e2d1c50235da8203826fd60
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-10 11:23:08 +0200

    Fixed the structure for Landmarks

--- apps/foundation/pages/landmarks/index.vue
+++ apps/foundation/pages/landmarks/index.vue
@@ -1,13 +1,19 @@
 <template lang="pug">
-VList(grid="~ col gap-6")
-  div(grid="~ col" b="solid purple 1")
-    HeaderTitle(:index="13" b="solid white 1")
-    ClaimHorizontal(b="solid green 1")
-  HeaderTitle(:index="14" b="solid red 1")
-  div(grid="~ col" b="solid pink 1")
-    Card(b="solid blue 1")
-      h3 No landmarks available
-  HeaderTitle(:index="15" b="solid yellow 1")
+VList()
+div()
+  HeaderTitle(:index="13")
+  HList()
+    Card()
+    Card()
+      MetashareClaimHorizontal()
+HeaderTitle(:index="14")
+Card() No landmarks available
+Div()
+  HeaderTitle(:index="15")
+    
+
+
+  
 
 </template>
 
