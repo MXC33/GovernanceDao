6968bd69904fa7c38af348287471ad700fb378d4
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-10 16:40:25 +0200

    Update index.vue in landmarks page.
    Removed old code and replaced with new page section/card. Added inputs and options which i will make into a own component, for now we have the skeleton for landmarks page.

--- apps/foundation/pages/landmarks/index.vue
+++ apps/foundation/pages/landmarks/index.vue
@@ -1,22 +1,32 @@
 <template lang="pug">
-VList()
-div()
-  HeaderTitle(:index="13")
+Page()
+  PageSection(section="rewards" :primary="true")
   HList()
     Card()
-    Card()
-      MetashareClaimHorizontal()
-HeaderTitle(:index="14")
-Card() No landmarks available
-Div()
-  HeaderTitle(:index="15")
-    
-
-
-  
-
-</template>
+    MetashareClaimHorizontal()
+  PageSection(section="myLandmarks")
+  card()
+    p No landmarks available
+  PageSection(section="explore")
+    div.input-container
+      input(type="text" v-model="searchQuery" placeholder="Search...")
+      select(v-model="filterOption")
+        option(value="" disabled selected) Filter by...
+        option(value="option1") Option 1
+        option(value="option2") Option 2
+        option(value="option3") Option 3
+      select(v-model="sortOption")
+        option(value="" disabled selected) Sort by...
+        option(value="asc") Ascending
+        option(value="desc") Descending
+  </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+const searchQuery = ref('');
+const filterOption = ref('');
+const sortOption = ref('');
+
 
 </script>
