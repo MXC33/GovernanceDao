73e378a6dada49088d5692a688dd8d8a95b45bbf
Ippe <felipehipsman@hotmail.com>
2023-10-12 09:13:27 +0200

    Update Landmark.vue
    Added hard coded array for Landmark page to render multiple boxes per each array item.'
    Will Re-do them to lang-file

--- apps/foundation/components/Card/Landmark.vue
+++ apps/foundation/components/Card/Landmark.vue
@@ -1,30 +1,48 @@
-<template lang="pug">
-VList(w="fit")
-  Card(rounded="tl tr")
-    h1(text="5xl") IMAGE
-  Card(bg="black" rounded="bl br")
-    VList()
-      TitleDetail()
-        template(#default) monumentals
-      HList()
+<template lang="pug"> 
+div(grid="~ cols-3 gap-6" flex-grow)
+  VList(v-for="item in titlar" :key="item.title" h="full")
+    Card(rounded="tl tr")
+      h1(text="5xl") IMAGE
+    Card(bg="black" rounded="bl br" flex-grow)
+      VList(flex-grow )
         TitleDetail()
-          template(#detail) 
-            ul(grid="~ cols-3")
-              li() shares
-              li() owners
-              li() earning diameter
+          template(#default ) {{ item.title }}
+        HList()
           TitleDetail()
-            template(#default) 
+            template(#detail) 
               ul(grid="~ cols-3")
-                li() 0
-                li() NA
-                li() 100
-    button(btn="~ primary-outline") view
-  
-  
+                li() shares
+                li() owners
+                li() earning diameter
+            TitleDetail()
+              template(#default) 
+                ul(grid="~ cols-3")
+                  li() 0
+                  li() NA
+                  li() 100
+      button(btn="~ primary-outline") iew
 </template>
   
 <script lang="ts" setup>
+
+interface Titles {
+  title: string
+}
+
+const titlar: Titles[] = [{
+  title: "Monumental Arch of a Faux-Celestial",
+},
+{
+  title: "Sealed Structure of Communion's Wake",
+
+},
+{
+  title: "Staff of Worship",
+},
+{
+  title: "Staff of Alberto",
+}
+]
 
 
 
