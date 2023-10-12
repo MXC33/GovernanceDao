7315096ef23ac353bc69b078f9e4853885da36de
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-12 13:39:37 +0200

    quick update
    small update for card/earnings and landmarks

--- apps/foundation/components/Card/Earnings.vue
+++ apps/foundation/components/Card/Earnings.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
 Card()
-  HList()
-    TitleDetail(grid="~ cols-2")
+  HList(grid="~ cols-2")
+    TitleDetail()
+
       template(#detail)
-        ul(style="list-style-type: none; padding: 0; margin: 0; display: flex; justify-content: space-between;" space-x="3")
+        ul(style="display: flex; justify-content: space-between;" space-x="3")
           li Earnings per day
           li Earnings 7 day
-      div(style="display: flex; justify-content: space-between;")
+      div(style="display: flex; justify-content: space-between")
         p 0.00
         p 0.00
   </template>
