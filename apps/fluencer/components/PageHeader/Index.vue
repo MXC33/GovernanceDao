2e103dc3d35d2e93562e837516f2a4c180e3b664
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-10-16 16:09:19 +0200

    Add page header

---
+++ apps/fluencer/components/PageHeader/Index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+VList(flex-grow="1" px="0 md:6" pos="relative" h="25vh" justify="center" items="center")
+  .gradient-bg(pos="fixed left-0 right-0 top-12 md:top-16" h="50vh")
+
+  VList(pos="relative" text="center" justify="center" items="center")
+    img(src="~/assets/images/bundle-cover.png" w="24")
+
+    h1(text="ix-orange xl") CONGRATULATIONS ON YOUR PURCHASE
+
+</template>
+
+<script lang="ts" setup>
+
+
+</script>
+
+<style>
+.gradient-bg {
+  background: rgb(12, 12, 12);
+  background: linear-gradient(0deg, rgba(12, 12, 12, 0.5) 50%, rgba(203, 0, 16, 0.5) 100%);
+}
+
+.gradient-bg-account {
+  background: rgb(12, 12, 12);
+  background: linear-gradient(0deg, rgba(12, 12, 12, 0.5) 50%, rgba(99, 99, 99, 0.5) 100%);
+}
+</style>
--- apps/fluencer/layouts/default.vue
+++ apps/fluencer/layouts/default.vue
@@ -3,6 +3,8 @@ main(w="full" h="full" flex="~ col grow" min-h="100vh")
   Header(:auto-close="true")
 
   VList(flex-grow="1" justify="top")
+    PageHeader()
+
     //- Used for teleports
     div(id="overlays" select="none")
 
