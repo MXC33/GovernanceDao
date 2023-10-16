fbf240f39a046ef1a1308a1b1836cb9ae3c517a0
Ippe <felipehipsman@hotmail.com>
2023-10-16 09:58:50 +0200

    Energy Page Update
    Started on /energy page section. Need to sort out pagesection which are not currently correct.

--- apps/foundation/components/Page/Section.vue
+++ apps/foundation/components/Page/Section.vue
@@ -4,7 +4,7 @@ VList(space-y="6")
     h1(v-if="primary" text="7xl" font="bold druk-medium") {{$t(`${String(route.name)}.${section}.title`) }}
     h2(v-else text="5xl" font="bold") {{$t(`${String(route.name)}.${section}.title`) }}
 
-    p(text="xl") {{$t(`${String(route.name)}.${section}.description`) }}
+    p(text="2xl") {{$t(`${String(route.name)}.${section}.description`) }}
 
   slot
 </template>
