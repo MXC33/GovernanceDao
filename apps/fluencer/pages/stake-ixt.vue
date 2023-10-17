4ae636e65284b4bada59e7c8d2dc7ea09c8a0fd1
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-10-17 11:46:30 +0200

    Clean up ixt prices

--- apps/fluencer/pages/stake-ixt.vue
+++ apps/fluencer/pages/stake-ixt.vue
@@ -9,7 +9,7 @@ Page()
   template(#default)
     PageTabs()
 
-    DrawerContent(:start-open="true" :is-neutral="true" bg="gray-900")
+    DrawerContent(:start-open="true" :is-neutral="true" bg="gray-900" max-h="auto")
       template(#header)
         div Stake your IXT
 
@@ -19,37 +19,86 @@ Page()
             template(#image)
               img(src="~/assets/images/ixt-coin.png")
             template(#title)
-              span(color="gray-200" text="md") My balance
+              span(text="detail") My balance
 
             template(#description)
-              VList(text="lg")
-                div(color="white" font="bold") 
-                  span {{ixtBalanceRounded}} IXT 
-                  span(font="400" color="gray-200") ${{usdBalanceRounded}}
+              div
+                span(text="subheading") {{ixtBalanceRounded}} IXT 
+                span(text="detail") ${{usdBalanceRounded}}
+
+          Divider(mx="-6")
+
+          VList(space-y="3")
+            PageSubTitle() Duration
+
+            div(grid="~ cols-4 gap-3")
+              button(btn="~ form on-active:form-active" v-for="period in stakePeriods" capitalize="~" :active="activePeriod.months == period.months" @click="selectPeriod(period)" flex="~ col") 
+                div(font="bold") {{ formattedMonths(period.months) }}
+                div(text="detail") {{ period.apy }}%
 
           Divider(mx="-6")
 
           VList()
-            PageSubTitle() Duration
-            div(grid="~ cols-4 gap-3")
-              button(btn="~ form on-active:form-active" v-for="monthCount in months" capitalize="~" :active="activeMonth == monthCount" @click="selectDays(dayCount)") {{ formattedDays(dayCount) }}
+            PriceRow()
+              template(#left): span(text="detail") Quantity
+              template(#right): span(text="detail-value") 2000 IXT
 
-          Divider(mx="-6")
+            PriceRow()
+              template(#left): span(text="detail") Staking period
+              template(#right)
+                div(text="detail-value")
+                  GlitchText(:text="formattedMonths(activePeriod.months)")
 
-          PageSubTitle() Projected Total
+            PriceRow()
+              template(#left): span(text="detail") APY
+              template(#right)
+                div(text="detail-value")
+                  GlitchText(:text="activePeriod.apy + '%'")
 
+          PriceRow()
+            template(#left)
+              PageSubTitle() Projected Total
 
-    
-    
+            template(#right) 
+              h3(space-x="2")
+                span(text="subheading") 250 IXT
+                span(text="detail") $200
+
 </template>
 
 
 <script lang="ts" setup>
-const months = [1, 3, 6, 12]
-const activeMonth = ref(1)
-const selectMonth = (active: number) => {
-
+interface StakePeriod {
+  months: number
+  apy: number
 }
+const stakePeriods: StakePeriod[] = [{
+  months: 1,
+  apy: 10.74
+}, {
+  months: 3,
+  apy: 15.22
+}, {
+  months: 6,
+  apy: 20.99
+}, {
+  months: 12,
+  apy: 42.01
+}]
+
+const activePeriod = ref<StakePeriod>(stakePeriods[0])
+
+const selectPeriod = (active: StakePeriod) => {
+  activePeriod.value = active
+}
+
+const formattedMonths = (months: number) => {
+  if (months == 12)
+    return "1 Year"
+
+  return `${months} Month`
+}
+
 
 const { ixtToUSD } = useCurrencyConversion()
 const { ixtBalance } = useCurrencyData()
