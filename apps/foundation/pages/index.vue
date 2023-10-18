71563e0093e7ff7472db7c71adf00550dfa569d2
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-18 16:24:31 +0200

    Updated Homepage
    Added some extra crisp to the menu buttons on homepage

--- apps/foundation/pages/index.vue
+++ apps/foundation/pages/index.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
 Page()
-  HList(grid="~ gap-6")
-    router-link(to="/territories" tag="button" btn="~ secondary") Territories Staking
-    router-link(to="/energy" tag="button" btn="~ secondary") Energy Staking
-    router-link(to="/landmarks" tag="button" btn="~ secondary") Landmarks Staking
-    router-link(to="/metashare" tag="button" btn="~ secondary") MetaShare Staking
-    router-link(to="/governance" tag="button" btn="~ secondary") Governance  
+  Card(flex-grow="1")
+    Div(grid="~ cols-5 gap-6 ")
+      ButtonGlitch(@click="navigateTo('/territories')" tag="button" btn="~ form" text="Territories Staking" font="bold") 
+      ButtonGlitch(@click="navigateTo('/energy')" tag="button" btn="~ form" text="Energy Staking" font="bold") 
+      ButtonGlitch(@click="navigateTo('/landmarks')" tag="button" btn="~ form" text="Landmark Staking" font="bold") 
+      ButtonGlitch(@click="navigateTo('/metashare')" tag="button" btn="~ form" text="Metashare Staking" font="bold") 
+      ButtonGlitch(@click="navigateTo('/Governance')" tag="button" btn="~ form" text="Governance" font="bold")   
 
   PageSection(section="dashboardStaking" :primary="true")
 
---
+++ layers/ix-base/components/Button/Glitch.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+button(ref="button")
+  GlitchText(:text="text" :hover="true" :is-hovered="isHovered")
+  </template>
+    
+<script lang="ts" setup>
+const button = ref()
+const isHovered = useElementHover(button)
+defineProps<{
+  text: string,
+}>()
+</script>
