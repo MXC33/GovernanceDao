d06317c4dce716118b5c58153e6e5ed4e04559dd
Ippe <felipehipsman@hotmail.com>
2023-10-19 14:14:55 +0200

    ClaimHorizontal now has text rendered from /lang file

--- apps/foundation/components/Claim/Horizontal.vue
+++ apps/foundation/components/Claim/Horizontal.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
 HList(flex flex-row justify-between)
   TitleDetail()
-    template(#detail) Claim rewards
-    template(#default) Rewards: [dynamic value]
+    template(#detail)
+      PageParagraphs(section="claimRewards")
+    template(#default) 0
+      // img(src="~/assets/images/token.svg")
   button(btn="~ primary-outline" disable="active") CLAIM REWARDS 
 </template>
 
