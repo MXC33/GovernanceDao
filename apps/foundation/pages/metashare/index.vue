e84dbf9c0b42f85513e57f550749b198ffd3247a
Ippe <felipehipsman@hotmail.com>
2023-10-20 14:02:50 +0200

    Fixed more lang strings

--- apps/foundation/pages/metashare/index.vue
+++ apps/foundation/pages/metashare/index.vue
@@ -1,23 +1,20 @@
 <template lang="pug">
 Page()
   ButtonNav() 
-  PageSection(section="staking" :primary="true")
+  PageSection(section="StakingTitle" :primary="true")
     Card()
       Table()
       ClaimHorizontal()
 
-  PageSection(section="corporation")
+  PageSection(section="CorporationTitle")
     div(grid="~ cols-2 gap-6")
       MetashareCorporation(v-for="stakingItem in data?.stakingItems" :staking-data="stakingItem")
+
   PageSection(section="corpPerformance")
     Table()
-  
-
-
 </template>
 
 <script lang="ts" setup>
-
 import { StakingId } from '../../.nuxt/gql/default'
 
 const { execute: fetchStakingData, data } = useStakingData(StakingId.Metashare)
@@ -27,12 +24,3 @@ console.log("data", data.value?.stakingItems)
 
 
 </script>
-
-<style>
-.DIVENSMORSA {
-  margin: 24px 0px;
-  display: flex;
-  gap: 48px 24px;
-  flex-wrap: wrap;
-}
-</style>
