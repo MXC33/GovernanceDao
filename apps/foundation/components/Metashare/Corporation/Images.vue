5df2c71bee7c6fee7b25da79caa9f6ad2eaea36a
Ippe <felipehipsman@hotmail.com>
2023-10-20 14:03:49 +0200

    Lang string changes

--- apps/foundation/components/Metashare/Corporation/Images.vue
+++ apps/foundation/components/Metashare/Corporation/Images.vue
@@ -4,4 +4,6 @@ div
 </template>
   
 <script lang="ts" setup>
+import { defineProps } from 'vue';
+
 </script>
--- apps/foundation/components/Metashare/Corporation/Index.vue
+++ apps/foundation/components/Metashare/Corporation/Index.vue
@@ -9,6 +9,16 @@ Card()
 <script lang="ts" setup>
 import type { StakingItemFragment } from '#gql';
 
+const imagePaths = [
+  "~/assets/images/corporations/AstroCorp.png",
+  "~/assets/images/corporations/ElabCorp.png",
+  "~/assets/images/corporations/GgCorp.png",
+  "~/assets/images/corporations/LuckCatCorp.png",
+  "~/assets/images/corporations/NetCorp.png",
+  "~/assets/images/corporations/NewLandsCorp.png",
+  "~/assets/images/corporations/Y_Logo.png",
+  // ... add more paths as needed
+];
 
 const { stakingData } = defineProps<{
   stakingData: StakingItemFragment | undefined
