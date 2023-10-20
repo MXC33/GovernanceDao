8ad382d95ecb69add48b3c8158b14159bc41d108
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-20 14:21:30 +0200

    Update Horizontal.vue
    added some logic so that the component renders in two pages.

--- apps/foundation/components/Claim/Horizontal.vue
+++ apps/foundation/components/Claim/Horizontal.vue
@@ -2,12 +2,32 @@
 HList(flex flex-row justify-between)
   TitleDetail()
     template(#detail)
-      PageParagraphs(section="claimRewards")
+      PageParagraphs(:section="currentSection")
     template(#default) 0
       // img(src="~/assets/images/token.svg")
-  button(btn="~ primary-outline" disable="active") CLAIM REWARDS 
-</template>
-
+  button(btn="~ primary-outline" disable="active") {{ currentButtonText }}
+  </template>
+  
 <script lang="ts" setup>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const currentSection = computed(() => {
+  if (route.path === '/') {
+    return 'claimRewards';
+  } else if (route.path === '/metashare') {
+    return 'TotalRewards';
+  }
+});
+
+const currentButtonText = computed(() => {
+  if (route.path === '/metashare') {
+    return 'CLAIM REWARDS';
+  } else if (route.path === '/') {
+    return 'CLAIM ALL';
+  }
+});
 
 </script>
