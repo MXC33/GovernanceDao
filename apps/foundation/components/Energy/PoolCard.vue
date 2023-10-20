740e8490242be56968d34a7323b84315a46ed21e
Ippe <felipehipsman@hotmail.com>
2023-10-20 11:11:17 +0200

    Energy page fixes
    Changed strings to match /lang file
    
    Added styling for the Energy Pool card.

--- apps/foundation/components/Energy/PoolCard.vue
+++ apps/foundation/components/Energy/PoolCard.vue
@@ -1,26 +1,51 @@
 <template lang="pug">
-Card()
-  VList()
-    TitleDetail()
-      h3() Energy Pool: 400 000 IXT / month
-      h4(color="") Epoch ends in 21D 6H 0M
-        TitleDetail()
-          template(#detail)
-            ul() 
-              li() Daily IXT per Energy
-              li() TVL 
-              li() IXT unlocked this epoch
-              li() Your Stake 
-              li() Claimable rewards
-          ul() 
-            li() 0
-            li() 0 
-            li() 0
-            li() 0
-            li() 0
-  button(btn="~ primary-outline") CHOOSE STAKING POOL
+Card
+  div
+    div(text-2xl font-bold) 
+      PageParagraphs(section="EnergyPool")
+    div(color="ix-orange" flex gap-1)
+      PageParagraphs(section="EpochEnds")
+      p 17D 5H 18M
+  div(grid="~ cols-3")
+    div 
+      div(text="detail")
+        PageParagraphs(section="DailyIXT")
+      div(style="display: flex; align-items: center;")
+        span 0.002
+        IXTIcon.icon(w="5" class="ml-2")
+    div 
+      div(text="detail") 
+        PageParagraphs(section="TVL")
+      div(style="display: flex; align-items: center;")
+        span 123
+        IXTIcon.icon(w="5" class="ml-2")
+    div 
+      div(text="detail") 
+        PageParagraphs(section="IXTUnlocked")
+      div(style="display: flex; align-items: center;")
+        span 123
+        IXTIcon.icon(w="5" class="ml-2")
+    div 
+      div(text="detail") 
+        PageParagraphs(section="YourStake")
+      div(style="display: flex; align-items: center;")
+        span 123
+        IXTIcon.icon(w="5" class="ml-2")
+    div 
+      div(text="detail") 
+        PageParagraphs(section="ClaimableRewards")
+      div(style="display: flex; align-items: center;")
+        span 123
+        IXTIcon.icon(w="5" class="ml-2")
+  HList(gap-6)
+  template(#detailBottom)
+    button(btn="~ primary-outline") 
+      PageParagraphs(section="ChooseYourPool")
 </template>
 
 <script lang="ts" setup>
+import IXTIcon from "~/assets/images/token.svg"
 
 </script>
+
+<style></style>
