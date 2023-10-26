c99a1726adc57066b89cf770dc5c6a57f79187a8
Ippe <felipehipsman@hotmail.com>
2023-10-26 14:21:16 +0200

    Added API data to landmark page

--- apps/foundation/pages/landmarks/index.vue
+++ apps/foundation/pages/landmarks/index.vue
@@ -2,9 +2,22 @@
 Page()
   ButtonNav()
   PageSection(section="Rewards")
-    HList(grid="~ gap-5")
-      CardEarnings()
-      ClaimTotalReward()
+    div(grid="~ gap-6 lg:cols-2")
+      HList(col="lg:col-span-1" space-x="6") 
+        Card(flex-grow="1")
+          HList(justify="between")
+            TitleDetail(icon="ixt")
+              template(#detail) Earning per day
+              template(#default) {{ roundToDecimals(landmarkData?.userSpecificStakingData?.totalUserRewardPerDay, 4) }}
+            TitleDetail(icon="ixt")
+              template(#detail) Earning 30 day
+              template(#default) {{ roundToDecimals(landmarkData?.userSpecificStakingData?.totalUserRewardPerThirtyDays, 4) }}
+        Card(flex-grow="1")
+          HList( justify="between")
+            TitleDetail(icon="ixt")
+              template(#detail) Total Rewards
+              template(#default) {{ roundToDecimals(landmarkData?.userSpecificStakingData?.totalUserReward, 4) }}
+            ButtonGlitch(btn="~ primary-outline-cut" @click="$emit('claim')" :text="$t('general.claim')")
   PageSection(section="MyLandmarks")
   card(text-center)
     PageParagraphs(section="NoLandmarksPlaceholder")
@@ -19,7 +32,22 @@ Page()
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+
+import { type StakingItemFragment, StakingId } from '@ix/base/.nuxt/gql/default';
+
+const activeSection = ref('area');
+const activeSectionTop = ref('area');
+const sections = ['area', 'sector', 'zone', 'domain'];
+
+const { data: landmarkData } = useStakingData(StakingId.Landmark)
+
+
+
+
+const { data } = defineProps<{
+  data: StakingItemFragment
+}>()
+
 
 const filterOptions = ['Option 1', 'Option 2', 'Option 3'];
 const sortOptions = ['Ascending', 'Descending'];
