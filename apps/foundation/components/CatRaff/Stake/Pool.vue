510e3fc8236482d5ed1139c387605d6b1d9e6d04
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-26 15:53:17 +0200

    Fixed a structure for CatRaff

---
+++ apps/foundation/components/CatRaff/Stake/Pool.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+div(grid="~ gap-6 lg:cols-2" p="4")
+  Card()
+    HList() 
+      div
+        VList(space-y="2")
+
+    HList(space-x="4" pt="4")
+      div()
+
+  Card()
+    HList() 
+      div
+        VList(space-y="2")
+
+    HList(space-x="4" pt="4")
+      div()
+  Card()
+    HList() 
+      div
+        VList(space-y="2")
+
+
+    HList(space-x="4" pt="4")
+      div()
+          
+
+  </template>
+    
+<script lang="ts" setup>
+
+</script>
