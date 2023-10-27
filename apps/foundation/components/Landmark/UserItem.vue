1402bb761a8deface43df109c27106e9a7f4d678
Ippe <felipehipsman@hotmail.com>
2023-10-27 14:26:05 +0200

    MyLandmark layout fixes

--- apps/foundation/components/Landmark/UserItem.vue
+++ apps/foundation/components/Landmark/UserItem.vue
@@ -1,22 +1,22 @@
 <template lang="pug"> 
 HList(v-for="item in data")
   Card(flex-grow="1")
-    HList()
-      TokenMedia(:token="item.token" w=100)
-      VList(justify="between")
+    div(grid="~ cols-4 gap-12")
+      TokenMedia(:token="item.token" grid="col-span-2" w=100)
+      VList()
         CardTitle() {{ item.token.tokenInfo?.title }}
-        TitleDetail()
-          template(#detail) Shares Owned
-          template(#default) {{ sharesOwned(item) }}
-        TitleDetail()
-          template(#detail) Shares Staked
-          template(#default) {{ sharesStaked(item) }}
+          TitleDetail()
+            template(#detail) Shares Owned
+            template(#default) {{ sharesOwned(item) }}
+          TitleDetail()
+            template(#detail) Shares Staked
+            template(#default) {{ sharesStaked(item) }}
 
+    template(#detailRight)  
       TitleDetail()
         template(#detail) Total Earned
         timplate(#default) {{ item.userStakingData?.totalReward }}
-
-      ButtonGlitch(btn="~ primary-outline-cut" @click="$emit('claim')" :text="$t('landmarks.claimButton')")
+          ButtonGlitch(btn="~ primary-outline-cut" @click="$emit('claim')" :text="$t('landmarks.claimButton')")
 </template>
   
 <script lang="ts" setup>
