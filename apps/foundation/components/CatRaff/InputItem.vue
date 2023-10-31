733e58190cbceb9300b6fc0e59138cd2e77e47a6
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-31 18:38:22 +0100

    Update InputItem.vue
    fixed the upper half of cat raff inputitem and stakingItem

--- apps/foundation/components/CatRaff/InputItem.vue
+++ apps/foundation/components/CatRaff/InputItem.vue
@@ -1,22 +1,20 @@
 <template lang="pug">
 Card()
   HList()
-    CardTitle() 
+    CardTitle(flex-grow space-y="2") 
       template(#default) {{ $t(`ixtCatRaff.${id}.title`) }}
       template(#detail) {{ $t(`ixtCatRaff.${id}.description`) }}
-    HList(space-x="6")
-      div() Available 
-      div() ixt
-  HList()
-    input()
-    ButtonGlitch(btn="~ primary-outline-cut" @click="$emit('withdraw')" text="button")
+    HList(space-x="1")
+      div() Available:
+      TitleDetail(:icon="id === 'stake' ? 'ixt' : 'ixt'")
+      div() 0.00
+  HList(space-x="2")
+    input(type="text" text="bold" :placeholder="id === 'stake' ? 'IXT' : 'TICKETS'" ref="searchElement" bg="gray-900 md:ix-black"  outline="none" px="2" w="full" size="3")
+    ButtonGlitch(btn="~ primary-outline-cut" :text="id === 'stake' ? 'STAKE IXT' : 'ADD TICKETS'")
 </template>
-    
+
 <script lang="ts" setup>
-
-
 const { id } = defineProps<{
-  id: string
-}>()
-
+  id: string;
+}>();
 </script>
