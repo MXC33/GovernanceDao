0557aeba8ab4d090637399190dc72f523ada46dc
goerris <gustav.seffer@gmail.com>
2023-10-31 14:34:10 +0100

    Fix total dashboard table

--- apps/foundation/components/Homepage/Dashboard/Table.vue
+++ apps/foundation/components/Homepage/Dashboard/Table.vue
@@ -6,6 +6,20 @@ Card()
       HList(items="center" space-x="3") 
         img(:src="`/assets/staking/${dashboardTableData[index].id}.png`" w="6" class="lg:block hidden")
         div(capitalize="~" ) {{ dashboardTableData[index].id }}
+
+
+    template(#footer-col-asset) 
+      span( text="gray-300") Total
+
+    template(#footer-col-daily) 
+      HList(items="center" space-x="1" font="bold")
+        div() {{ roundToDecimals(totalDaily) }}
+        Icon(icon="ixt")
+
+    template(#footer-col-30days) 
+      HList(items="center" space-x="1" font="bold")
+        div {{ roundToDecimals(total30Day) }}
+        Icon(icon="ixt")
 </template>
   
   
@@ -14,6 +28,9 @@ import type { SimpleTableRow, SimpleTableColumn } from '@ix/base/composables/use
 
 const { dashboardTableData } = useStakingPools()
 
+const totalDaily = computed(() => dashboardTableData.value.reduce((a, b) => a + b.daily, 0))
+
+const total30Day = computed(() => dashboardTableData.value.reduce((a, b) => a + b.thirtyDays, 0))
 
 const rows = computed<SimpleTableRow[]>(() =>
   dashboardTableData.value.map((item) => ({
@@ -26,8 +43,8 @@ const rows = computed<SimpleTableRow[]>(() =>
 )
 const columns: SimpleTableColumn[] = [
   { title: "Asset", id: "asset" },
-  { title: "1 day", },
-  { title: "30 days", },
+  { title: "1 day", id: "daily" },
+  { title: "30 days", id: "30days" },
 ]
 
 
