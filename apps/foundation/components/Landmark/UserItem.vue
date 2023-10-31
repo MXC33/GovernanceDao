2b13f83164e4fd1fff3332614c9782be81a3cb35
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-31 14:12:37 +0100

    Updated Landmarks
    fixed some layout issues on "my landmarks" cards

--- apps/foundation/components/Landmark/UserItem.vue
+++ apps/foundation/components/Landmark/UserItem.vue
@@ -1,26 +1,28 @@
 <template lang="pug"> 
 HList(v-for="item in data")
   Card(flex-grow="1")
-    div(grid="~ cols-3" )
-      TokenMedia(:token="item.token" grid="col-1" w=50)
+    div(grid="~ cols-3")
+      TokenMedia(:token="item.token" w=50)
       VList()
         CardTitle() {{ item.token.tokenInfo?.title }}
-          TitleDetail(space-y="1")
+          TitleDetail(my="3")
             template(#detail) Shares Owned
             template(#default) {{ sharesOwned(item) }}
-          TitleDetail(space-y="1")
+          TitleDetail(my="3")
             template(#detail) Shares Staked
             template(#default) {{ sharesStaked(item) }}
-          HList(space-x="3")
+          HList(space-x="5" mt="10")
             ButtonGlitch(btn="~ primary-outline-cut" @click="$emit('deposit')" :text="$t('landmarks.StakeBtn.title')")
             ButtonGlitch(btn="~ primary-outline-cut" @click="$emit('withdraw')" :text="$t('landmarks.UnstakeBtn.title')")
 
 
-    template(#detailRight)
-      TitleDetail(space-y="3")
+    template(#blackDetail)
+      TitleDetail(space-y="3" mt="25")
         template(#detail) Total Earned
-        template(#default :style="{ fontSize: '24px', marginBottom: '10px' }") {{ item.userStakingData?.totalReward }}
-          ButtonGlitch(btn="~ primary-outline-cut" @click="claimReward(item)" :text="$t('landmarks.claimButton')")
+        template(#default) 
+          div {{ item.userStakingData?.totalReward }}
+          ButtonGlitch(btn="~ primary-outline-cut" mt="3" @click="claimReward(item)" :text="$t('landmarks.claimButton')")
+
 
 
 </template>
