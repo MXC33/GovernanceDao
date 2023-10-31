74a0fad12fabfb30662b38447544a30b6a43a86f
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-10-31 11:50:45 +0100

    Updated treasury index
    fixed the content in the blackboxes for the pool stats.
    Minor issues with layout on the text.

--- apps/foundation/components/CatRaff/Treasury/Index.vue
+++ apps/foundation/components/CatRaff/Treasury/Index.vue
@@ -13,19 +13,21 @@ div(grid="~ cols-2" gap-6)
         template(#detail) {{ $t(`ixtCatRaff.PoolPrice.description`) }}
         template(#price) $ 51,134.943
 
-  //höger
-  VList()
-    Card()
-      CardTitle()
-        template(#default) {{ $t(`ixtCatRaff.PoolStats.title`) }}
-      CardDetail()
-        CardTitle()
-          template(#default) 0 IXT
-          template(#detail) {{ $t(`ixtCatRaff.CurrentWeekDeposits.title`) }}
-      CardDetail()
-        CardTitle()
-          template(#detail) {{ $t(`ixtCatRaff.TotalWeekDeposits.title`) }}
 
+  Card(flex-grow)
+    CardTitle()
+      template(#default) {{ $t(`ixtCatRaff.PoolStats.title`) }}
+    div(bg="black" flex-grow style="display: flex; align-items: center; justify-content: space-between;")
+      CardTitle(flex="~ row justify-between")
+        div()
+          TitleDetail(#detail) {{ $t(`ixtCatRaff.CurrentWeekDeposits.title`) }}
+        div()
+          span 0 IXT
+          span ($0)
+    HList(bg="black" flex-grow style="display: flex; align-items: center; justify-content: space-between;")
+      CardTitle()
+        TitleDetail(#detail) {{ $t(`ixtCatRaff.TotalWeekDeposits.title`) }}
+        div 136779 IXT
 </template>
 
 <script lang="ts" setup>
