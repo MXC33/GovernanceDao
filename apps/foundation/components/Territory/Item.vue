463600fffa6ce49828a63560998d43477cb6b29c
goerris <gustav.seffer@gmail.com>
2023-10-31 14:01:53 +0100

    fix unstaked territorys showing and filtering

--- apps/foundation/components/Territory/Item.vue
+++ apps/foundation/components/Territory/Item.vue
@@ -1,34 +1,38 @@
 <template lang="pug">
 Card()
 
-  TokenMedia(:token="data.token" h="60" pos="relative")
+  TokenVideo(:token="token" h="60" pos="relative")
 
   Divider(mx="-6")
 
   div(grid="~ cols-2" space-x="3")
     TitleDetail(icon="ixt") 
       template(#detail) {{ $t('general.rewards') }}
-      template(#default) {{ roundToDecimals(data.userStakingData?.totalReward) }}
+      template(#default) {{ roundToDecimals(totalReward) }}
 
-    ButtonGlitch(btn="~ primary-outline-cut" @click="claimReward(data)" :text="$t('general.claim')")
+    ButtonGlitch(btn="~ primary-outline-cut on-disabled:disabled" @click="claimReward(data)" :text="$t('general.claim')" :disabled="!staked")
 
-  TerritoryStake(:item="data")
+  TerritoryStake(:item="data" :staked="staked")
   
 </template>
 
 <script lang="ts" setup>
-import type { StakingItemFragment } from '#gql'
+import type { StakingItemFragment, NftFragment } from '#gql'
 
 const { claimSpecificTerritoryReward } = useTerritoryStakingContract()
 
-const { data } = defineProps<{
-  data: StakingItemFragment
+const { data, staked } = defineProps<{
+  staked: boolean
+  data: StakingItemFragment | NftFragment
 }>()
+
+const totalReward = computed(() => staked ? data.userStakingData?.totalReward : 0)
+
+const token = computed(() => staked ? data.token : data)
 
 const claimReward = (item: StakingItemFragment) => {
   return claimSpecificTerritoryReward(item.token)
 }
 
 
-console.log("data", data.token)
 </script>
