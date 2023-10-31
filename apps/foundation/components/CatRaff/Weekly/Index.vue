2f56a78e0743c5c9eee56896f6ba3f5a34482823
Ippe <felipehipsman@hotmail.com>
2023-10-31 15:44:31 +0100

    Added API data to past winnings
    Also made them go from last win to most early.

--- apps/foundation/components/CatRaff/Weekly/Index.vue
+++ apps/foundation/components/CatRaff/Weekly/Index.vue
@@ -1,46 +1,37 @@
 <template lang="pug">
 div(overflow-x="auto" whitespace-nowrap)
-  div(inline="flex" space-x="6")
-    div(v-for="item in items" :key="item.id" w="300px") 
+  div(inline="flex" space-x="6" flex-row-reverse)
+    div(v-for="(week, index) in data "  w="300px") 
       Card()
         VList()
-          TitleDetail(#default mb="6") Week
-          TitleDetail(#detail) Oct 25, 2023
-          TitleDetail(#detail) 00:00AM GMT
+          TitleDetail(#default mb="6") Week # {{week.week}}
+          TitleDetail(#detail) Thursday
+          TitleDetail(#detail) {{ calculateEndDate(index) }}
           TitleDetail(#detail) 2500 IXT
         template(#detailBottom)
           VList()
             div(grid="~ cols-2")
               div()
-                TitleDetail(#default) 321823
+                TitleDetail(#default) {{ week.totalTickets }}
                 TitleDetail(#detail) Total Tickets
               div()
-                TitleDetail(#default) 321823
-                TitleDetail(#detail) Total Tickets
+                TitleDetail(#default) {{week.userEnteredTicket}}
+                TitleDetail(#detail) Your Tickets
 </template>
 	
 <script lang="ts" setup>
+import { type CatRaffTicketInfoFragment } from '#gql';
 import { defineProps } from 'vue';
+import { format } from 'date-fns';
 
-const props = defineProps({
-  weeklyData: {
-    type: Array as () => Array<{ id: number }>,
-    required: false
-  }
-});
+const startDate = 1657670400000
+const sevenDaysSeconds = 604800000
 
 
+const calculateEndDate = (index: number) => format(startDate + (sevenDaysSeconds * index+1), 'dd-MM-yyyy')
 
-const items = [
-  { id: 1, content: 'Item 1' },
-  { id: 2, content: 'Item 2' },
-  { id: 3, content: 'Item 3' },
-  { id: 4, content: 'Item 4' },
-  { id: 5, content: 'Item 5' },
-  { id: 6, content: 'Item 6' },
-  { id: 7, content: 'Item 7' },
-  { id: 8, content: 'Item 8' },
-  { id: 9, content: 'Item 9' },
-];
 
+defineProps<{
+  data: CatRaffTicketInfoFragment[]
+}>()
 </script>
