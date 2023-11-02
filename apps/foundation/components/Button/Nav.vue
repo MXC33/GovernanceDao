fb96e8fda48b41e4ceb8961b15319a2c17541ff6
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-11-02 16:49:48 +0100

    Update Nav.vue
    Fixed so the button nav is mobile friendly

--- apps/foundation/components/Button/Nav.vue
+++ apps/foundation/components/Button/Nav.vue
@@ -1,13 +1,15 @@
 <template lang="pug">
-div(grid="~ cols-6 gap-3" mb="8" px="6")
+div(grid="~ cols-2 lg:cols-6 gap-3" mb="8" px="6")
   ButtonGlitch(@click="navigateTo('/')" btn="~ form" text="Home" font="bold")
   ButtonGlitch(@click="navigateTo('/territories')" btn="~ form" text="Territories Staking" font="bold") 
 
   ButtonGlitch(@click="navigateTo('/landmarks')" btn="~ form" text="Landmark Staking" font="bold") 
   ButtonGlitch(@click="navigateTo('/metashare')" btn="~ form" text="Metashare Staking" font="bold") 
+
   ButtonGlitch(@click="navigateTo('/energy')" btn="~ form" text="Energy Staking" font="bold") 
   ButtonGlitch(@click="navigateTo('/ixtCatRaff')" btn="~ form" text="Cat Raff" font="bold")   
 </template>
+  
 
 <script lang="ts" setup>
 
