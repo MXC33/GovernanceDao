de5ba7532620d296fbb7849bda5324fd079ce7e5
MaxCid <113206163+MaxCid@users.noreply.github.com>
2023-12-06 17:44:43 +0100

    Update IxtStaking.vue
    adding wallet balance, staking balance and inputs

--- apps/foundation/components/Governance/Card/IxtStaking.vue
+++ apps/foundation/components/Governance/Card/IxtStaking.vue
@@ -1,11 +1,28 @@
 <template lang="pug">
 Card()
-  HList(space-x="6")
+  HList(space-x="6" items="center")
     PageSection(section="ixtStaking")
-    ButtonGlitch(btn="~ primary-outline-cut"  text="Ixt Staking")
-  HList(justify="between")
-    div() total Ixt Staking
-    div() Voting Ixt Staking:
+    Card(flex="")
+        TitleDetail(icon="ixt")
+          template(#default) {{ roundToDecimals(ixtBalance, 2) }}
+          template(#detail) Wallet IXT Balance
+    
+  Card(space-x="3")
+    HList(space-x="1")
+      div() Available:
+      TitleDetail(:icon="'ixt'")
+    HList(space-x="2")
+      input(type="number" text="bold" :placeholder="'IXT'" bg="gray-900 md:ix-black" px="2" w="full" v-model="amount")
+      Disabler(:disabled="ixtBalance == 0")
+    HList()
+      ButtonGlitch(btn="~ primary-outline-cut" @click="onClick" :text="'STAKE IXT'")
+      ButtonGlitch(btn="~ primary-outline-cut" @click="onClick" :text="'WITHDRAW IXT'")
+  HList(border="solid white 1")
+      div() Available:
+        TitleDetail(:icon="'ixt'")
+
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { ixtBalance } = useCurrencyData();
+</script>
